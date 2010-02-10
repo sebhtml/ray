0423285f179936c4edb1bbce20b0c80f841a113e
Sébastien Boisvert <seb@boisvert.info>
2010-02-10 20:09:14 +0000

    Added the direction class.
    SB

---
+++ Direction.cpp
@@ -0,0 +1,46 @@
+/*
+ 	Ray
+    Copyright (C) 2010  Sébastien Boisvert
+
+	http://DeNovoAssembler.SourceForge.Net/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You have received a copy of the GNU General Public License
+    along with this program (gpl-3.0.txt).  
+	see <http://www.gnu.org/licenses/>
+
+*/
+
+#include"Direction.h"
+#include<stdlib.h>
+
+void Direction::constructor(int wave,int progression){
+	m_wave=wave;
+	m_progression=progression;
+	m_next=NULL;
+}
+
+int Direction::getWave(){
+	return m_wave;
+}
+
+int Direction::getProgression(){
+	return m_progression;
+}
+
+Direction*Direction::getNext(){
+	return m_next;
+}
+
+void Direction::setNext(Direction*e){
+	m_next=e;
+}
+
---
+++ Direction.h
@@ -0,0 +1,38 @@
+/*
+ 	Ray
+    Copyright (C) 2010  Sébastien Boisvert
+
+	http://DeNovoAssembler.SourceForge.Net/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You have received a copy of the GNU General Public License
+    along with this program (gpl-3.0.txt).  
+	see <http://www.gnu.org/licenses/>
+
+*/
+
+
+#ifndef Direction
+#define _Direction
+class Direction{
+	int m_wave;
+	int m_progression;
+	Direction*m_next;
+public:
+	void constructor(int wave,int progression);
+	int getWave();
+	int getProgression();
+	Direction*getNext();
+	void setNext(Direction*e);
+};
+
+#endif
+
