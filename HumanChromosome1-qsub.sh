2f4ae7eeee8141b3ec388bb770aa7caad1dde75d
Sébastien Boisvert <seb@boisvert.info>
2010-02-22 18:46:34 +0000

    Removed the qsub SGE submission script. It does not belong there.
    Sébastien Boisvert.

--- HumanChromosome1-qsub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -N Chromosome1
-#$ -P nne-790-aa
-#$ -l h_rt=24:00:00
-#$ -pe hosts 64
-module load compilers/gcc/4.1.2 mpi/openmpi/1.4.1_gcc 
-/software/MPI/openmpi-1.4.1_gcc/bin/mpirun /home/sboisver12/Ray/trunk/Ray
-/home/sboisver12/nne-790-aa/Ray-input.txt
-
