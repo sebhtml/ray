4a08a820e6d9fde5f8e69bb54cd565f7a84b7011
Sébastien Boisvert <seb@boisvert.info>
2010-03-30 23:22:35 +0000

    Modified script.

--- scripts/dump-results.sh
+++ scripts/dump-results.sh
@@ -3,12 +3,13 @@ for i in $(cat sets.txt|grep 'o '|awk '{print $2}')
 do
 	echo "========"
 	grep $i sets.txt
+	echo "%  & numberOfContigs & bases & meanSize  & n50  & max   & coverage   & misassembled & mismatches & indels"
 	for j in newbler euler-sr velvet ray abyss
 	do
 		file=0$i-$j.sh.res
 		if test -f $file
 		then
-			cat $file
+			cat $file|grep -v EDT|grep -v numberOf
 		else
 			echo "MISSING RESULTS ($j)"
 		fi
