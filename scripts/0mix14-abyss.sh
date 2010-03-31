70d0c1f06104d0d6eccf08d88fbe17f03c14b2ba
Sébastien Boisvert <seb@boisvert.info>
2010-03-31 14:24:02 +0000

    Corrected scripts.

--- scripts/0mix14-abyss.sh
+++ scripts/0mix14-abyss.sh
@@ -4,5 +4,5 @@ source ../0mix11-parameters.sh
 source ../0parameters.sh
 source ../0mix13-parameters.sh
 
-abyss-pe k=$wordSize n=10  name=test lib="lib1 lib2" lib1="$p1left $p1right" lib2="$p2left $p2right" se="$r4541 $r4542 $r4543" &> log1
+abyss-pe k=$wordSize n=50  name=test lib="lib1 lib2" lib1="$p1left $p1right" lib2="$p2left $p2right" se="$r4541 $r4542 $r4543" &> log1
 print-latex.sh $ref test-contigs.fa ABySS
