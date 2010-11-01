423b283406f54c6a0a9c0418e2c7369234489bef
Sébastien Boisvert <seb@boisvert.info>
2010-11-01 03:34:58 +0000

    Updating scripts.

--- examples/run-chr1-human.sh
+++ examples/run-chr1-human.sh
@@ -1,12 +1,12 @@
-nohup /software/openmpi-1.5/bin/mpirun -np 22 ./Ray \
- -s chr1-10.fa \
- -s chr1-1.fa \
- -s chr1-2.fa \
- -s chr1-3.fa \
- -s chr1-4.fa \
- -s chr1-5.fa \
- -s chr1-6.fa \
- -s chr1-7.fa \
- -s chr1-8.fa \
- -s chr1-9.fa
- -o chr1.fasta > chr1.fasta.log &
+nohup /software/openmpi-1.5/bin/mpirun -np 22 ../Ray \
+ -s /home/boiseb01/nuccore/chr1-10.fa \
+ -s /home/boiseb01/nuccore/chr1-1.fa \
+ -s /home/boiseb01/nuccore/chr1-2.fa \
+ -s /home/boiseb01/nuccore/chr1-3.fa \
+ -s /home/boiseb01/nuccore/chr1-4.fa \
+ -s /home/boiseb01/nuccore/chr1-5.fa \
+ -s /home/boiseb01/nuccore/chr1-6.fa \
+ -s /home/boiseb01/nuccore/chr1-7.fa \
+ -s /home/boiseb01/nuccore/chr1-8.fa \
+ -s /home/boiseb01/nuccore/chr1-9.fa
+ -o /home/boiseb01/nuccore/chr1.fasta > chr1.log &
