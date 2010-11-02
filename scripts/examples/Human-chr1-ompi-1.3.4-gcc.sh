58f2c7be3e73e073619efd6f6753c1ac858a8186
Sébastien Boisvert <seb@boisvert.info>
2010-11-02 20:33:09 +0000

    Added a script.

---
+++ scripts/examples/Human-chr1-ompi-1.3.4-gcc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -N Ray
+#$ -P nne-790-aa
+#$ -l h_rt=12:00:00
+#$ -pe mpi 256
+module load compilers/gcc/4.4.2 mpi/openmpi/1.3.4_gcc
+/software/MPI/openmpi-1.3.4_gcc/bin/mpirun /home/sboisver12/Ray/tags/0.0.7/Ray /home/sboisver12/nne-790-aa/colosse.clumeq.ca/qsub/Ray-input.txt
