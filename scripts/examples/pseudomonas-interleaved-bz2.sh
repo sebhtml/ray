f003cf495854f7494c6d82e7e0f1fbb60ee7d282
Sébastien Boisvert <seb@boisvert.info>
2010-11-21 21:22:16 +0000

    Adding a file.

---
+++ scripts/examples/pseudomonas-interleaved-bz2.sh
@@ -0,0 +1,13 @@
+/software/mpich2-1.3/bin/mpirun -np 30 \
+~/Ray/trunk/code/Ray \
+-i /home/boiseb01/nuccore/inter.fasta.bz2 \
+-o Pseudomonas.fasta \
+|& tee pseudomonas.log
+
+ls -lh pseudomonas.log
+
+~/Ray/trunk/scripts/print-latex.sh \
+~/nuccore/Pseudomonas-aeruginosa-PAO1\,-complete-genome.fasta \
+Pseudomonas.fasta \
+RayTheMighty
+
