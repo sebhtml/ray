2804231008079e225d350fded44b88e26706b913
Sébastien Boisvert <seb@boisvert.info>
2010-11-21 20:45:29 +0000

    added +1

--- code/Library.cpp
+++ code/Library.cpp
@@ -54,7 +54,7 @@ void Library::updateDistances(){
 				int standardDeviation=(*m_parameters).getObservedStandardDeviation(library);
 				if((*m_sequence_idInFile)<(*m_parameters).getNumberOfSequences(*m_fileId)){
 					if((*m_sequence_idInFile)%100000==0){
-						cout<<"Rank 0 is updating distances "<<(*m_sequence_idInFile)<<"/"<<(*m_parameters).getNumberOfSequences(*m_fileId)<<endl;
+						cout<<"Rank 0 is updating distances "<<(*m_sequence_idInFile)+1<<"/"<<(*m_parameters).getNumberOfSequences(*m_fileId)<<endl;
 					}
 					int sequenceRank=(*m_sequence_id)%getSize();
 					int sequenceIndex=(*m_sequence_id)/getSize();
