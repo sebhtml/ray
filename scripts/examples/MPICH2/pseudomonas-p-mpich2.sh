0df5cd68bd331afa582af964e4cc8161688e74b6
Sébastien Boisvert <seb@boisvert.info>
2010-11-22 21:55:46 +0000

    Modified scripts.

--- scripts/examples/MPICH2/pseudomonas-p-mpich2.sh
+++ scripts/examples/MPICH2/pseudomonas-p-mpich2.sh
@@ -1,7 +1,7 @@
 /software/mpich2-1.3/bin/mpirun -np 31 ~/Ray/trunk/code/Ray \
--p ~/nuccore/Pseud,200b,2x50b,50X_1.fasta.bz2 ~/nuccore/Pseud,200b,2x50b,50X_2.fasta.gz \
--o pseudo.fasta |& tee pseudomonas-p-bz2-gz.log
+-p ~/nuccore/Pseud,200b,2x50b,50X_1.fasta ~/nuccore/Pseud,200b,2x50b,50X_2.fasta \
+-o pseudo.fasta |& tee pseudomonas-p.log
 
 ~/Ray/trunk/scripts/print-latex.sh \
-~/nuccore/Pseudomonas-aeruginosa-PAO1,-complete-genome.fasta pseudo.fasta Ray | tee -a  pseudomonas-p-bz2-gz.log
+~/nuccore/Pseudomonas-aeruginosa-PAO1,-complete-genome.fasta pseudo.fasta Ray | tee -a  pseudomonas-p.log
 
---
+++ scripts/examples/MPICH2/pseudomonas-s-mpich2.sh
@@ -0,0 +1,8 @@
+/software/mpich2-1.3/bin/mpirun -np 31 ~/Ray/trunk/code/Ray \
+-s ~/nuccore/Pseud,200b,2x50b,50X_1.fasta \
+-s ~/nuccore/Pseud,200b,2x50b,50X_2.fasta \
+-o pseudo.fasta |& tee pseudomonas-s-mpich2.log
+
+~/Ray/trunk/scripts/print-latex.sh \
+~/nuccore/Pseudomonas-aeruginosa-PAO1,-complete-genome.fasta pseudo.fasta Ray | tee -a  pseudomonas-s-mpich2.log
+
