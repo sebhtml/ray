860692574f088011d1104ac9d424cd91b8677a42
Sébastien Boisvert <seb@boisvert.info>
2010-11-22 13:58:27 +0000

    Add/Change #581 (run Ray on Pseudomonas -p)

--- scripts/examples/pseudomonas-p-bz2-gz.sh
+++ scripts/examples/pseudomonas-p-bz2-gz.sh
@@ -1,5 +1,5 @@
 /software/mpich2-1.3/bin/mpirun -np 31 ~/Ray/trunk/code/Ray \
--p Pseud,200b,2x50b,50X_1.fasta.bz2 Pseud,200b,2x50b,50X_2.fasta.gz \
+-p ~/nuccore/Pseud,200b,2x50b,50X_1.fasta.bz2 ~/nuccore/Pseud,200b,2x50b,50X_2.fasta.gz \
 -o pseudo.fasta |& tee pseudomonas-p-bz2-gz.log
 
 ~/Ray/trunk/scripts/print-latex.sh \
