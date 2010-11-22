47ee904b7561489ed9e4e6718fc4c662d2a6ab27
Sébastien Boisvert <seb@boisvert.info>
2010-11-22 02:20:23 +0000

    Add/Change #575 (run Ray with -i on ls30 with MPICH2 & SRA001125)

--- scripts/interleave-fastq.py
+++ scripts/interleave-fastq.py
@@ -9,20 +9,17 @@ f1=open(file1)
 f2=open(file2)
 
 while True:
-	line1a=f1.readline()
-	if len(line1a)==0:
+	line=f1.readline()
+	if len(line)==0:
 		break
-	line1b=f1.readline()
-	f1.readline()
-	f1.readline()
-	line2a=f2.readline()
-	line2b=f2.readline()
-	f2.readline()
-	f2.readline()
-	print line1a.strip()
-	print line1b.strip()
-	print line2a.strip()
-	print line2b.strip()
+	print line
+	i=0
+	while i<3:
+		print f1.readline()
+	
+	i=0
+	while i<4:
+		print f2.readline()
 
 f2.close()
 f1.close()
