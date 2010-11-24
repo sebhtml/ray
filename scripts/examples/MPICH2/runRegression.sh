70a217b629660cd65b7d09ac42e8f17645783853
Sébastien Boisvert <seb@boisvert.info>
2010-11-24 21:37:19 +0000

    adding regression.

---
+++ scripts/examples/MPICH2/runRegression.sh
@@ -0,0 +1,24 @@
+$i=$1
+$suffix=$2
+
+(
+echo "RegressionTest"
+echo ""
+echo "FILE = $i"
+date
+echo ""
+echo "OH HAI"
+echo ""
+echo "======== BEGIN >>>"
+echo ""
+bash $i
+echo ""
+echo "======== END^^ >>>"
+echo ""
+echo "K THX BYE"
+echo ""
+echo "..."
+date
+echo ""
+)|tee $i-$suffix
+
--- scripts/examples/MPICH2/runRegressions.sh
+++ scripts/examples/MPICH2/runRegressions.sh
@@ -1,41 +1,20 @@
 
-log=Regressions-$(date +%Y-%m-%d-%H-%M-%S-%s).txt
+suffix=$(date +%Y-%m-%d-%H-%M-%S-%s).txt
 
 (
-j=1
 echo "BEGIN LIST OF REGRESSIONS"
 for i in $(ls *.sh|grep -v runRegressions)
 do
 	echo "RegressionTest Number $j -> $i"
-	j=$(($j+1))
 done
 echo "END^^ LIST OF REGRESSIONS"
 echo ""
 echo ""
 
-j=1
 
 for i in $(ls *.sh|grep -v runRegressions)
 do
-	echo "RegressionTest Number $j -> $i"
-	j=$(($j+1))
-	echo ""
-	echo "FILE = $i"
-	date
-	echo ""
-	echo "OH HAI"
-	echo ""
-	echo "======== BEGIN >>>"
-	echo ""
-	bash $i
-	echo ""
-	echo "======== END^^ >>>"
-	echo ""
-	echo "K THX BYE"
-	echo ""
-	echo "..."
-	date
-	echo ""
+	bash runRegression.sh $i $suffix
 done 
 
-) |tee $log
+) |tee Regressions-$suffix
