37578ce8e432033c9aac8e40bd8fe9072d38953d
Sébastien Boisvert <seb@boisvert.info>
2011-02-05 18:49:14 +0000

    Updated scripts.

--- scripts/examples/ray.sh
+++ scripts/examples/ray.sh
@@ -1,4 +1,4 @@
 mpirun -tag-output -np 31 ~/Ray/trunk/code/Ray \
 -p /home/boiseb01/nuccore/Large-Ecoli/200_1.fastq /home/boiseb01/nuccore/Large-Ecoli/200_2.fastq -p /home/boiseb01/nuccore/Large-Ecoli/1000_1.fastq /home/boiseb01/nuccore/Large-Ecoli/1000_2.fastq -p /home/boiseb01/nuccore/Large-Ecoli/4000_1.fastq /home/boiseb01/nuccore/Large-Ecoli/4000_2.fastq -p /home/boiseb01/nuccore/Large-Ecoli/10000_1.fastq /home/boiseb01/nuccore/Large-Ecoli/10000_2.fastq 
 
-print-latex.sh /home/boiseb01/nuccore/Ecoli-k12-mg1655.fasta  RayOutput.fasta Ray
+print-latex.sh /home/boiseb01/nuccore/Ecoli-k12-mg1655.fasta  RayOutput.fasta $0.Ray
