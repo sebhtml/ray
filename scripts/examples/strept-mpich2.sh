c933825b1ce56be4aa66d9e7a3137b0950bdcd87
Sébastien Boisvert <seb@boisvert.info>
2011-05-12 19:36:00 +0000

    Add/Change #267 (add a flag to show the ending context)

--- scripts/examples/strept-mpich2.sh
+++ scripts/examples/strept-mpich2.sh
@@ -1,7 +1,7 @@
 mpirun -tag-output -np $(cat PROCESSES) ~/Ray/trunk/code/Ray \
 -p /home/boiseb01/nuccore/200xStreptococcus-pneumoniae-R6.fasta_fragments_1.fasta \
    /home/boiseb01/nuccore/200xStreptococcus-pneumoniae-R6.fasta_fragments_2.fasta \
--debug-bubbles \
+-show-ending-context \
 -o $0
 
 ~/Ray/trunk/scripts/print-latex.sh ~/nuccore/Streptococcus-pneumoniae-R6.fasta $0.fasta $0.Ray
