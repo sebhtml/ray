d9aa2ed5a324abe72c2093722dbf6042ecea58ca
Sébastien Boisvert <seb@boisvert.info>
2011-05-20 21:04:44 +0000

    Updating manual;

--- scripts/examples/L1-L3.sh
+++ scripts/examples/L1-L3.sh
@@ -2,5 +2,5 @@ mpirun -tag-output -np $(cat PROCESSES) ~/Ray/trunk/code/Ray \
  -p ~/RECOMB-seq-2011/L1_1.fasta ~/RECOMB-seq-2011/L1_2.fasta -p ~/RECOMB-seq-2011/L2_1.fasta ~/RECOMB-seq-2011/L2_2.fasta \
  -p ~/RECOMB-seq-2011/L3_1.fasta ~/RECOMB-seq-2011/L3_2.fasta -o $0
 
-~/Ray/trunk/scripts/print-latex.sh ~/nuccore/Ecoli-k12-mg1655.fasta $0.fasta $0.Ray
+~/Ray/trunk/scripts/print-latex.sh ~/nuccore/Ecoli-k12-mg1655.fasta $0.Contigs.fasta $0.Ray
 
