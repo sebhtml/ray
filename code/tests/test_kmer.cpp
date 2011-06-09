fbb9fef984e26d9134511cefb796a8c646bcddd1
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-06-08 23:23:00 -0400

    I catched the renegade with my handy test suite -- I will now fix it.
    a.out: tests/test_kmer.cpp:48: void test_Ingoing(): Assertion `actual==aKmer' failed.
    
    Expected:
    
    0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20
    G  A  C  T  T  G  A  T  T  A  G  A  C  A  A  G  A  A  G  T  T
    0  2  4  6  8  10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40
    11 00 10 01 01 11 00 01 01 00 11 00 10 00 00 11 00 00 11 01 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    Actual:
    G  A  C  T  T  G  A  T  T  A  G  A  C  A  A  G  A  A  G  T  T
    0  2  4  6  8  10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42
    11 00 10 01 01 11 00 01 01 00 11 00 10 00 00 11 00 00 11 01 01 11 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    
                                     These folks should be 0s----> **
    
    Otherwise, while the string representation, the hashing value may change...

--- code/tests/test_kmer.cpp
+++ code/tests/test_kmer.cpp
@@ -8,6 +8,46 @@
 #include <iostream>
 using namespace std;
 
+void test_Ingoing(){
+	string a="GACTTGATTAGACAAGAAGTT";
+	string b= "ACTTGATTAGACAAGAAGTTG";
+	int wordSize=a.length();
+
+	// description of m_edges:
+	// outgoing  ingoing
+	//
+	// G C T A G C T A
+	//
+	// 7 6 5 4 3 2 1 0
+	
+	uint8_t edges=(1<<3);
+
+	Kmer aKmer=wordId(a.c_str());
+	Kmer bKmer=wordId(b.c_str());
+	
+	vector<Kmer>inEdges=_getIngoingEdges(&bKmer,edges,wordSize);
+	Kmer actual=inEdges[0];
+	string actualStr=idToWord(&actual,wordSize);
+	if(actualStr!=a){
+		cout<<"MAXKMERLENGTH: "<<MAXKMERLENGTH<<endl;
+		cout<<"WordSize: "<<wordSize<<endl;
+		cout<<"Expected"<<endl;
+		cout<<a<<" -> "<<b<<endl;
+		cout<<"Actual:"<<endl;
+		cout<<actualStr<<" -> "<<b<<endl;
+		cout<<endl;
+	}
+	assertEquals(actualStr,a);
+
+	if(actual!=aKmer){
+		cout<<"Expected: "<<endl;
+		aKmer.print();
+		cout<<"Actual: "<<endl;
+		actual.print();
+	}
+	assert(actual==aKmer);
+}
+
 
 int main(int argc,char**argv){
 	//cout<<endl;
@@ -65,7 +105,19 @@ int main(int argc,char**argv){
 	for(int i=0;i<(int)inEdges.size();i++){
 		//cout<<"Entry "<<i<<" "<<endl;
 		//inEdges[i].print();
-		string a=idToWord(&(inEdges[i]),wordSize);
+		Kmer theKmer=inEdges[i];
+		string a=idToWord(&theKmer,wordSize);
+		Kmer id=wordId(a.c_str());
+
+		if(theKmer!=id){
+			cout<<"Expected: "<<endl;
+			id.print();
+			cout<<"Actual: "<<endl;
+			theKmer.print();
+		}
+
+		assert(id==theKmer);
+
 		assertEquals(tmp.count(a),0);
 		tmp.insert(a);
 		assertEquals(a.substr(1,wordSize-1),seq.substr(0,wordSize-1));
@@ -77,8 +129,17 @@ int main(int argc,char**argv){
 	assertEquals(4,outEdges.size());
 	tmp.clear();
 	for(int i=0;i<(int)outEdges.size();i++){
-		string a=idToWord(&(outEdges[i]),wordSize);
+		Kmer theKmer=outEdges[i];
+		string a=idToWord(&theKmer,wordSize);
 
+		Kmer id=wordId(a.c_str());// make sure that all bit are set to 0 except those relevant 
+		if(theKmer!=id){
+			cout<<"Expected: "<<endl;
+			id.print();
+			cout<<"Actual: "<<endl;
+			theKmer.print();
+		}
+		assert(theKmer==id);
 		assertEquals(tmp.count(a),0);
 		tmp.insert(a);
 		assertEquals(seq.substr(1,wordSize-1),a.substr(0,wordSize-1));
@@ -86,7 +147,8 @@ int main(int argc,char**argv){
 	}
 
 	assertEquals(tmp.size(),4);
-	
+
+	test_Ingoing();
 	return 0;
 }
 
