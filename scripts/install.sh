074372077160f7a5f6475956f63a8040b1a0c244
sebhtml <seb@boisvert.info>
2011-06-10 16:07:11 -0400

    Really *fixed* installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,10 @@ echo ""
 echo "Installing Ray to $PREFIX"
 echo ""
 cp $TARGETS $PREFIX
-cp InstructionManual.* $PREFIX
+if test -f InstructionManual.pdf
+then
+	cp InstructionManual.pdf $PREFIX
+fi
 cp README.md $PREFIX/README.doc
 cp LICENSE $PREFIX/LICENSE.doc
 cp ChangeLog $PREFIX/ChangeLog.doc
