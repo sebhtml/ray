9b38e4e3520b3db0eb82160db66cc0e7716a382f
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-06-17 20:13:01 -0400

    Added a unit test for uniformity of vertices on ranks.

--- tests/test_invert.sh
+++ tests/test_invert.sh
@@ -1,4 +1,4 @@
 CODE=../code
 
-g++ -I. -I$CODE $CODE/core/common_functions.cpp test_invert.cpp -g $CODE/cryptography/crypto.cpp -DMAXKMERLENGTH=32 $CODE/structures/Kmer.cpp -I ..
+g++ -I. $CODE/memory/malloc_types.cpp -I$CODE $CODE/core/common_functions.cpp test_invert.cpp -g $CODE/cryptography/crypto.cpp -DMAXKMERLENGTH=32 $CODE/structures/Kmer.cpp -I ..
 ./a.out
--- tests/test_uniform.cpp
+++ tests/test_uniform.cpp
@@ -1,20 +1,65 @@
 #include <core/common_functions.h>
 #include <structures/Kmer.h>
+#include <map>
+#include <stdlib.h>
 #include <time.h>
 #include <iostream>
 using namespace std;
 
-int main(){
+int computeAverage(vector<int>*a){
+	uint64_t sum;
+	for(int i=0;i<(int)a->size();i++)
+		sum+=a->at(i);
+	return sum/a->size();
+}
+
+void f2(){
 	srand(time(NULL));
 	int size=1536;
 	map<int,int> counts;
 	
-	uint64_t samples=1000000;
+	uint64_t samples=100000000;
 	uint64_t base=rand();
 	int wordSize=63;
 	Kmer kmer;
 	kmer.setU64(0,base);
 
+	int average=samples/size;
+	while(samples--){
+		uint64_t second=rand();
+		kmer.setU64(1,second);
+		int rank=vertexRank(&kmer,size,wordSize);
+		counts[rank]++;
+	}
+	vector<int> data;
+	for(int i=0;i<size;i++){
+		data.push_back(counts[i]);
+		//cout<<i<<" "<<counts[i]<<endl;
+	}
+	int deviation=average/10;
+	int min=average-deviation;
+	int max=average+deviation;
+	for(int i=0;i<size;i++){
+		if(counts[i]>=max){
+			cout<<counts[i]<<" and Max="<<max<<endl;
+		}
+		assert(counts[i]<max);
+		assert(counts[i]>min);
+	}
+}
+
+void f1(){
+	srand(time(NULL));
+	int size=1536;
+	map<int,int> counts;
+	
+	uint64_t samples=100000000;
+	uint64_t base=rand();
+	int wordSize=63;
+	Kmer kmer;
+	kmer.setU64(0,base);
+
+	int average=samples/size;
 	while(samples--){
 		uint64_t first=rand();
 		uint64_t second=rand();
@@ -23,8 +68,25 @@ int main(){
 		int rank=vertexRank(&kmer,size,wordSize);
 		counts[rank]++;
 	}
+	vector<int> data;
 	for(int i=0;i<size;i++){
-		cout<<i<<" "<<counts[i]<<endl;
+		data.push_back(counts[i]);
+		//cout<<i<<" "<<counts[i]<<endl;
 	}
+	int deviation=average/10;
+	int min=average-deviation;
+	int max=average+deviation;
+	for(int i=0;i<size;i++){
+		if(counts[i]>=max){
+			cout<<counts[i]<<" and Max="<<max<<endl;
+		}
+		assert(counts[i]<max);
+		assert(counts[i]>min);
+	}
+}
+
+int main(){
+	f1();
+	f2();
 	return EXIT_SUCCESS;
 }
