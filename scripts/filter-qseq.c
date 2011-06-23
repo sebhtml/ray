f33a60c770ce638ce99df4240d5f1cde97eb9a5b
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-06-23 18:28:18 -0400

    New C script.

---
+++ scripts/filter-qseq.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+takes 2 Illumina qseq file and only keep pairs that don't have the '.' character
+*/
+int main(int argc,char**argv){
+	char*file1=argv[1];
+	char*file1=argv[2];
+	char sequence1[4096];
+	char sequence2[4096];
+	FILE*fp1=fopen(file1,"r");
+	FILE*fp2=fopen(file2,"r");
+
+	fclose(fp2);
+	fclose(fp1);
+	return 0;
+}
