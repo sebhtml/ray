cc0d4111667865621c6cc9d13822de365f1437ba
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-07-11 22:27:31 -0400

    Added a system test for the Illumina MiSeq.

---
+++ system-tests/tests/coli-MiSeq/main.sh
@@ -0,0 +1,6 @@
+mpirun -tag-output -np $NSLOTS $RAY_GIT_PATH/code/Ray  \
+-p MiSeq_Ecoli_MG1655_110527_R1.fastq \
+   MiSeq_Ecoli_MG1655_110527_R2.fastq \
+-o $TEST_NAME -show-memory-usage
+
+ValidateGenomeAssembly.sh Ecoli-k12-mg1655.fasta $TEST_NAME.Contigs.fasta $TEST_NAME.Ray
