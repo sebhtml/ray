20f6df74da5c792cffd446755795931cfd1e9953
Sébastien Boisvert <Sebastien.Boisvert@oicr.on.ca>
2011-07-14 13:37:15 -0400

    Added a working prototype in Python for calling library peaks.

---
+++ scripts/prototype-library-caller.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+#encoding: utf-8
+
+# analyze a PREFIX.LibraryX.txt file
+# this is a prototype.
+# Author: Sébastien Boisvert
+
+import sys
+
+file=sys.argv[1]
+
+print ""
+print file
+
+x=[]
+y=[]
+
+for line in open(file):
+	tokens=line.split()
+	x.append(int(tokens[0]))
+	y.append(int(tokens[1]))
+
+minX=x[0]
+maxX=x[len(x)-1]
+middle=(maxX-minX)/2
+
+i=0
+peakLeft=i
+while i<len(x) and x[i]<middle:
+	if y[i]>y[peakLeft]:
+		peakLeft=i
+	i+=1
+
+peakRight=i
+while i<len(x) and x[i]<=maxX:
+	if y[i]>y[peakRight]:
+		peakRight=i
+	i+=1
+
+def callPeak(x,y,peak,step):
+	first=x[peak]-step
+	last=x[peak]+step
+
+	i=0
+	sum=0
+	n=0
+	thresold=0.001
+	dataPoints=0
+	while i<len(x):
+		if x[i]>=first and x[i] <= last and y[i]> y[peak]*thresold:
+			sum+=x[i]*y[i]
+			n+=y[i]
+			dataPoints+=1
+		i+=1
+	average=sum/n
+
+	minimumDatapoints=2
+	if dataPoints<minimumDatapoints:
+		return
+
+	sum=0
+	i=0
+	while i<len(x):
+		if x[i]>=first and x[i] <= last and y[i]> y[peak]*thresold:
+			diff=(x[i]-average)
+			sum+=diff*diff*y[i]
+		i+=1
+	
+	standardDeviation=((sum+0.0)/n)**(0.5)
+
+	first=average-standardDeviation
+	last=average+standardDeviation
+	i=0
+	busy=0
+	while i<len(y):
+		if x[i]>=first and x[i] <= last:
+			busy+=1
+		i+=1
+
+	occupancy=(busy+0.0)/(last-first+1)*100
+
+	threshold=20
+	if occupancy<threshold:
+		return
+
+	print "Peak Average= "+str(average)+" StandardDeviation= "+str(standardDeviation)+" Count= "+str(n)+" Points= "+str(dataPoints)+" Occupancy= "+str(occupancy)+" %"
+
+step=2000
+if x[peakRight]<x[peakLeft]+step:
+	peak=peakLeft
+	if y[peakRight]>y[peakLeft]:
+		peak=peakRight
+	callPeak(x,y,peak,step)
+else:
+	callPeak(x,y,peakLeft,step)
+	callPeak(x,y,peakRight,step)
