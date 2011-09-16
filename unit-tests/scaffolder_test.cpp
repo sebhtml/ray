2828d85c2ec500b2faa13ae66734717a686ab418
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-09-16 13:16:10 -0400

    Added more debugging information in the scaffolding test.

--- unit-tests/scaffolder_test.cpp
+++ unit-tests/scaffolder_test.cpp
@@ -34,6 +34,12 @@ int main(int argc,char**argv){
 	f2.close();
 	}
 
+	map<uint64_t,int> lengths;
+
+	for(int i=0;i<(int)vertices.size();i++){
+		lengths[vertices[i].getName()]=vertices[i].getLength();
+	}
+
 	{
 	ifstream f(edgeFile.c_str());
 	int n=0;
@@ -65,6 +71,18 @@ int main(int argc,char**argv){
 	vector<vector<int> >m_scaffoldGaps;
 
 	solver.solve(&m_scaffoldContigs,&m_scaffoldStrands,&m_scaffoldGaps);
+
+	for(int i=0;i<(int)m_scaffoldContigs.size();i++){
+		cout<<endl;
+		cout<<"Scaffold # "<<i<<endl;
+		cout<<m_scaffoldContigs[i].size()<<" contigs"<<endl;
+		for(int j=0;j<(int)m_scaffoldContigs[i].size();j++){
+			cout<<j<<"	contig: "<<m_scaffoldContigs[i][j]<<"	strand: "<<m_scaffoldStrands[i][j]<<" length: "<<lengths[m_scaffoldContigs[i][j]]<<endl;
+			if(j!=(int)m_scaffoldContigs[i].size()-1){
+				cout<<"gap: "<<m_scaffoldGaps[i][j]<<endl;
+			}
+		}
+	}
 
 	return 0;
 }
