778e5b001b0ac7d5a645396cace7bcb4570c3b80
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-01-09 08:59:09 -0500

    Moved the license in the main directory.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,10 @@ echo "Installing Ray to $PREFIX"
 echo ""
 cp $TARGETS $PREFIX
 cp -r Documentation $PREFIX
+cp README.md $PREFIX
+cp MANUAL_PAGE.txt $PREFIX
+cp INSTALL.txt $PREFIX
+cp AUTHORS $PREFIX
+cp LICENSE.txt $PREFIX
 
 ls $PREFIX
