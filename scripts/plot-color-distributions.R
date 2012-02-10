e2e202f6281eea42db5ddd9864b10bc006670fea
Sébastien Boisvert <seb@boisvert.info>
2012-02-10 12:07:56 -0500

    Buggy correlations are now printed in color plots.

--- scripts/plot-color-distributions.R
+++ scripts/plot-color-distributions.R
@@ -8,6 +8,7 @@ d1=read.table(paste(prefix,".RawDistribution.tsv",sep=""),header=TRUE)
 d2=read.table(paste(prefix,".UniquelyColoredDistribution.tsv",sep=""),header=TRUE)
 d3=read.table(paste(prefix,".UniquelyColoredAssembledDistribution.tsv",sep=""),header=TRUE)
 
+
 # find the peak
 maxI=1
 i=1
@@ -33,10 +34,74 @@ if(i > length(d1[[1]])){
 
 maxX=d1[[1]][i]
 
+# generate non-sparse arrays
+
+d1Full=1:d1[[1]][length(d1[[1]])] * 0
+
+i=1
+while(i <= length(d1[[1]])){
+	x=d1[[1]][i]
+	y=d1[[2]][i]
+
+	if(x<=length(d1Full))
+		d1Full[x]=y
+
+	i=i+1
+}
+
+d2Full=1:d2[[1]][length(d2[[1]])] * 0
+
+i=1
+while(i <= length(d2[[1]])){
+	x=d2[[1]][i]
+	y=d2[[2]][i]
+
+	if(x<= length(d2Full))
+		d2Full[x]=y
+
+	i=i+1
+}
+
+
+d3Full=1:d3[[1]][length(d3[[1]])] * 0
+
+i=1
+while(i <= length(d3[[1]])){
+	x=d3[[1]][i]
+	y=d3[[2]][i]
+
+	if(x<=length(d3Full))
+		d3Full[x]=y
+
+	i=i+1
+}
+
+print("d2")
+print (d2Full)
+print("d3")
+print (d3Full)
+
+cor_d1_d2=cor(d1Full[1:length(d2Full)],d2Full)
+cor_d2_d3=cor(d2Full[1:length(d3Full)],d3Full)
+cor_d1_d3=cor(d1Full[1:length(d3Full)],d3Full)
+
 png(paste(prefix,".png",sep=""))
-plot(d1[[1]],d1[[2]],type='l',col='black',xlim=c(0,maxX),xlab='K-mer coverage depth',
-ylab='Frequency',main='Distributions of k-mer coverage depth',log='y')
-lines(d2[[1]],d2[[2]],col='blue')
-lines(d3[[1]],d3[[2]],col='green')
+plot(d1[[1]],d1[[2]],type='b',col='black',xlim=c(0,maxX),xlab='K-mer coverage depth',
+ylab='Frequency',main=paste("Distributions of k-mer coverage depth\n",
+" Correlation black-blue: ",cor_d1_d2,"\n",
+" Correlation black-green: ",cor_d1_d3,"\n",
+" Correlation blue-green: ",cor_d2_d3,sep=""),
+log='y')
+
+lines(d2[[1]],d2[[2]],col='blue',type='b')
+lines(d3[[1]],d3[[2]],col='green',type='b')
+
 dev.off()
 
+#png(paste(prefix,"-scattered-blue-green.png",sep=""))
+#plot(d2Full,d3Full)
+#dev.off()
+
+#png(paste(prefix,"-scattered-black-blue.png",sep=""))
+#plot(d1Full,d2Full)
+#dev.off()
