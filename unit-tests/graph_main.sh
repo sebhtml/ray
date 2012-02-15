2b7314098ad77e07c652711f6dc6307f51eeb793
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-02-14 20:46:17 -0500

    I updated the routing tests.

--- unit-tests/graph_main.sh
+++ unit-tests/graph_main.sh
@@ -16,9 +16,9 @@ graph_main.cpp \
 echo "Done compiling !"
 
 time ./graph_main 512 debruijn 8 1
-#time ./graph_main 512 debruijn 1
+time ./graph_main 512 debruijn 2 1
 #time ./graph_main 511 debruijn 1
-#time ./graph_main 320 kautz 1
+time ./graph_main 320 kautz 1 1
 #time ./graph_main 256 complete 1
 #time ./graph_main 128 random 1
 #time ./graph_main 200 group 1
