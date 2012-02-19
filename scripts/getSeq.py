d1a1b1d8de3ccc23f5e283e119ed0d1f70727288
Eric Normandeau - Bioinformatics <eric.normandeau.qc@gmail.com>
2012-02-19 15:48:10 -0500

    Updated script

--- scripts/getSeq.py
+++ scripts/getSeq.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
+"""Get a sequence from a fasta file by specifying the sequence name
 
+Usage:
+    getSeq.py fasta_file sequence_name
+"""
+
+# Importing modules
 import sys
 
-file=sys.argv[1]
-name=sys.argv[2]
+# Main
+if __name__ = '__main__':
+    try:
+        fasta_file = sys.argv[1]
+        sequence_name = sys.argv[2]
+    except:
+        print __doc__:
+        sys.exit(1)
 
-dump=False
+    dump = False
 
-for line in open(file):
-	if line[0]=='>':
-		if line.find(name)>=0:
-			dump=True
-		elif dump:
-			break
-	if dump:
-		print line.strip()
+    for line in open(fasta_file):
+        if line.startswith('>') and sequence_name in line:
+        # The previous condition is dangerous in the case of a sequence name
+        # that can be found as a substring of another name. See other option below:
+        
+        # if line.startswith('>') and line.strip().replace('>', '') == sequence_name:
+            dump = True
+        
+        if dump:
+            print line.strip()
+            break
