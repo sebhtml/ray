1b8e19ad36b7bdb47197aff870610d2fd2dadd80
Eric Normandeau - Bioinformatics <eric.normandeau.qc@gmail.com>
2012-02-19 15:27:36 -0500

    Updated script

--- scripts/interleave-fastq.py
+++ scripts/interleave-fastq.py
@@ -2,32 +2,35 @@
 # encoding:utf8
 # author: Sébastien Boisvert
 # part of Ray distribution
-# this script takes two fastq files and interleave them.
+"""This script takes two fastq files and interleaves them
 
+Usage:
+    interleave-fasta.py fasta_file1 fasta_file2
+"""
 
+# Importing modules
 import sys
 
-if len(sys.argv)==1:
-	print "Provide 2 fastq files..."
-	sys.exit()
+# Main
+if __name__ == '__main__':
+    try:
+        file1 = sys.argv[1]
+        file2 = sys.argv[2]
+    except:
+        print __doc__
+        sys.exit(1)
+    
+    with open(file1) as f1:
+        with open(file2) as f2:
+            while True:
+                line = f1.readline()
+                if line.strip() == "":
+                    break
+                print line.strip()
+                
+                for i in xrange(3):
+                    print f1.readline().strip()
+                
+                for i in xrange(4):
+                    print f2.readline().strip()
 
-f1=open(sys.argv[1])
-f2=open(sys.argv[2])
-
-while True:
-	line=f1.readline()
-	if len(line)==0:
-		break
-	print line.strip()
-	i=0
-	while i<3:
-		print f1.readline().strip()
-		i+=1
-	
-	i=0
-	while i<4:
-		print f2.readline().strip()
-		i+=1
-
-f2.close()
-f1.close()
