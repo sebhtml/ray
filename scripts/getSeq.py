6e1f59453d97b0dc1dd5a479fbc13b70249badc1
Sébastien Boisvert <seb@boisvert.info>
2012-02-28 17:53:40 -0500

    The code contributed by Éric Normandeau is broken and does not work. I restored the old code.

--- scripts/getSeq.py
+++ scripts/getSeq.py
@@ -8,25 +8,20 @@ Usage:
 # Importing modules
 import sys
 
-# Main
-if __name__ = '__main__':
-    try:
-        fasta_file = sys.argv[1]
-        sequence_name = sys.argv[2]
-    except:
-        print __doc__
-        sys.exit(1)
+if len(sys.argv)!=3:
+	print __doc__
+	sys.exit(1)
 
-    dump = False
+file=sys.argv[1]
+name=sys.argv[2]
 
-    for line in open(fasta_file):
-        if line.startswith('>') and sequence_name in line:
-        # The previous condition is dangerous in the case of a sequence name
-        # that can be found as a substring of another name. See other option below:
-        
-        # if line.startswith('>') and line.strip().replace('>', '') == sequence_name:
-            dump = True
-        
-        if dump:
-            print line.strip()
-            break
+dump=False
+
+for line in open(file):
+	if line[0]=='>':
+		if line.find(name)>=0:
+			dump=True
+		elif dump:
+			break
+	if dump:
+		print line.strip()
