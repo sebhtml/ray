sebhtml/ray

commit 19c1838382cda4bcc19dffcdc25745a076ffc38a

This is an experimental patch to accelerate one of the slowest computational steps of Ray. Basically, the patch tells Ray instances that completed previously to not try to do anything else after completion.

code/plugin_FusionTaskCreator/FusionTaskCreator.cpp
@@ -58,6 +58,12 @@ void FusionTaskCreator::initializeMethod(){
 
 /** finalize the whole thing */
 void FusionTaskCreator::finalizeMethod(){
+
+	if(m_finishedInPreviousCycle){
+
+		cout<<"Rank "<<m_parameters->getRank()<<" will not do anything, completion occured previously for fusing."<<endl;
+	}
+
 	/** all the paths */
 	int numberOfPaths=m_paths->size();
 
@@ -68,12 +74,17 @@ void FusionTaskCreator::finalizeMethod(){
 
 	bool removedPaths=false;
 	
-	if(eliminatedPaths>= 1)
+	if(eliminatedPaths>= 1){
 		removedPaths = true;
+	}
 
+	// nothing was eliminated here.
+	if(!removedPaths){
+		m_finishedInPreviousCycle=true;
+	}
 
 	cout<<"Rank "<<m_parameters->getRank()<<" FusionTaskCreator ["<<m_completedJobs<<"/"<<2*m_paths->size()<<"]"<<endl;
-	cout<<"Statistics: all paths: "<<numberOfPaths<<" eliminated: "<<eliminatedPaths<<endl;
+	cout<<"Statistics: all paths: "<<numberOfPaths<<" eliminated during fusing: "<<eliminatedPaths<<endl;
 
 	/* send a message */
 	uint64_t*message=(uint64_t*)m_outboxAllocator->allocate(sizeof(uint64_t));
@@ -93,6 +104,13 @@ void FusionTaskCreator::finalizeMethod(){
 
 /** has an unassigned task left to compute */
 bool FusionTaskCreator::hasUnassignedTask(){
+	
+	// we have nothing to do if we finished in the previous cycle.
+	if(m_finishedInPreviousCycle){
+
+		return false;
+	}
+
 	return m_iterator < (uint64_t)m_paths->size();
 }
 
@@ -182,6 +200,7 @@ void FusionTaskCreator::registerPlugin(ComputeCore*core){
 	RAY_MPI_TAG_DISTRIBUTE_FUSIONS_FINISHED_REPLY_REPLY=core->allocateMessageTagHandle(plugin);
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_DISTRIBUTE_FUSIONS_FINISHED_REPLY_REPLY,"RAY_MPI_TAG_DISTRIBUTE_FUSIONS_FINISHED_REPLY_REPLY");
 
+	m_finishedInPreviousCycle=false;
 }
 
 void FusionTaskCreator::resolveSymbols(ComputeCore*core){

code/plugin_FusionTaskCreator/FusionTaskCreator.h
@@ -70,6 +70,9 @@ class FusionTaskCreator: public TaskCreator, public CorePlugin {
 	uint64_t m_iterator;
 	uint64_t m_currentWorkerIdentifier;
 	bool m_reverseStrand;
+
+	bool m_finishedInPreviousCycle;
+
 public:
 	void constructor( VirtualProcessor*virtualProcessor,StaticVector*outbox,
 		RingAllocator*outboxAllocator,int*mode,Parameters*parameters,vector<vector<Kmer> >*paths,vector<uint64_t>*pathIdentifiers,

code/plugin_JoinerTaskCreator/JoinerTaskCreator.cpp
@@ -60,6 +60,12 @@ void JoinerTaskCreator::initializeMethod(){
 
 /** finalize the whole thing */
 void JoinerTaskCreator::finalizeMethod(){
+
+	if(m_previouslyDone){
+
+		cout<<"Rank "<<m_parameters->getRank()<<" will not do anything, completion occured previously for joining."<<endl;
+	}
+
 	/** all the paths */
 	int numberOfPaths=m_paths->size();
 
@@ -70,11 +76,17 @@ void JoinerTaskCreator::finalizeMethod(){
 
 	bool removedPaths=false;
 	
-	if(eliminatedPaths >= 1)
+	if(eliminatedPaths >= 1){
 		removedPaths = true;
+	}
+	
+	// nothing was eliminated
+	if(!removedPaths){
+		m_previouslyDone=true;
+	}
 
 	cout<<"Rank "<<m_parameters->getRank()<<" JoinerTaskCreator ["<<m_completedJobs<<"/"<<2*m_paths->size()<<"]"<<endl;
-	cout<<"Statistics: all paths: "<<numberOfPaths<<" eliminated: "<<eliminatedPaths<<endl;
+	cout<<"Statistics: all paths: "<<numberOfPaths<<" eliminated during joining: "<<eliminatedPaths<<endl;
 
 	/* send a message */
 	uint64_t*message=(uint64_t*)m_outboxAllocator->allocate(sizeof(uint64_t));
@@ -95,6 +107,11 @@ void JoinerTaskCreator::finalizeMethod(){
 
 /** has an unassigned task left to compute */
 bool JoinerTaskCreator::hasUnassignedTask(){
+
+	if(m_previouslyDone){
+		return false;
+	}
+
 	return m_iterator < (uint64_t)m_paths->size();
 }
 
@@ -180,6 +197,8 @@ void JoinerTaskCreator::registerPlugin(ComputeCore*core){
 	m_adapter_RAY_SLAVE_MODE_FINISH_FUSIONS.setObject(this);
 	core->setSlaveModeObjectHandler(plugin,RAY_SLAVE_MODE_FINISH_FUSIONS, &m_adapter_RAY_SLAVE_MODE_FINISH_FUSIONS);
 	core->setSlaveModeSymbol(plugin,RAY_SLAVE_MODE_FINISH_FUSIONS,"RAY_SLAVE_MODE_FINISH_FUSIONS");
+
+	m_previouslyDone=false;
 }
 
 void JoinerTaskCreator::resolveSymbols(ComputeCore*core){

code/plugin_JoinerTaskCreator/JoinerTaskCreator.h
@@ -70,6 +70,8 @@ class JoinerTaskCreator: public TaskCreator,  public CorePlugin{
 
 	vector<vector<Kmer> >*m_newPaths;
 
+	bool m_previouslyDone;
+
 public:
 	void constructor( VirtualProcessor*virtualProcessor,StaticVector*outbox,
 		RingAllocator*outboxAllocator,int*mode,Parameters*parameters,vector<vector<Kmer> >*paths,vector<uint64_t>*pathIdentifiers,

commit ae054e3944bbb731c7735fcde3bb5fdfc9502be9

This change set fixes a bug introduced in the commit 19c1838382cda4bcc19dffcdc25745a076ffc38a. This bug caused some contigs to be wrongly duplicated in the assembly. The bug was introduced on Sat Apr 21 23:22:21 2012 -0400. To solve it, the code path introduced is now disabled, but its effect remains because of the reduced number of cycles.
Paul H Roy and Maxime Déraspe reported this bug.

code/plugin_FusionTaskCreator/FusionTaskCreator.cpp
@@ -66,7 +66,7 @@ void FusionTaskCreator::initializeMethod(){
 /** finalize the whole thing */
 void FusionTaskCreator::finalizeMethod(){
 
-	if(m_finishedInPreviousCycle){
+	if(m_fastRun && m_finishedInPreviousCycle){
 
 		cout<<"Rank "<<m_parameters->getRank()<<" will not do anything, completion occured previously for fusing."<<endl;
 	}
@@ -113,7 +113,7 @@ void FusionTaskCreator::finalizeMethod(){
 bool FusionTaskCreator::hasUnassignedTask(){
 	
 	// we have nothing to do if we finished in the previous cycle.
-	if(m_finishedInPreviousCycle){
+	if(m_fastRun && m_finishedInPreviousCycle){
 
 		return false;
 	}
@@ -208,6 +208,9 @@ void FusionTaskCreator::registerPlugin(ComputeCore*core){
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_DISTRIBUTE_FUSIONS_FINISHED_REPLY_REPLY,"RAY_MPI_TAG_DISTRIBUTE_FUSIONS_FINISHED_REPLY_REPLY");
 
 	m_finishedInPreviousCycle=false;
+
+	// TODO: this is buggy
+	m_fastRun=false;
 }
 
 void FusionTaskCreator::resolveSymbols(ComputeCore*core){

code/plugin_FusionTaskCreator/FusionTaskCreator.h
@@ -81,6 +81,10 @@ class FusionTaskCreator: public TaskCreator, public CorePlugin {
 
 	bool m_finishedInPreviousCycle;
 
+	// fast run
+
+	bool m_fastRun;
+
 public:
 	void constructor( VirtualProcessor*virtualProcessor,StaticVector*outbox,
 		RingAllocator*outboxAllocator,int*mode,Parameters*parameters,vector<vector<Kmer> >*paths,vector<uint64_t>*pathIdentifiers,

code/plugin_JoinerTaskCreator/JoinerTaskCreator.cpp
@@ -68,7 +68,7 @@ void JoinerTaskCreator::initializeMethod(){
 /** finalize the whole thing */
 void JoinerTaskCreator::finalizeMethod(){
 
-	if(m_previouslyDone){
+	if(m_fastRun && m_previouslyDone){
 
 		cout<<"Rank "<<m_parameters->getRank()<<" will not do anything, completion occured previously for joining."<<endl;
 	}
@@ -115,7 +115,7 @@ void JoinerTaskCreator::finalizeMethod(){
 /** has an unassigned task left to compute */
 bool JoinerTaskCreator::hasUnassignedTask(){
 
-	if(m_previouslyDone){
+	if(m_fastRun && m_previouslyDone){
 		return false;
 	}
 
@@ -206,6 +206,9 @@ void JoinerTaskCreator::registerPlugin(ComputeCore*core){
 	core->setSlaveModeSymbol(plugin,RAY_SLAVE_MODE_FINISH_FUSIONS,"RAY_SLAVE_MODE_FINISH_FUSIONS");
 
 	m_previouslyDone=false;
+
+	// TODO this is buggy
+	m_fastRun=false;
 }
 
 void JoinerTaskCreator::resolveSymbols(ComputeCore*core){

code/plugin_JoinerTaskCreator/JoinerTaskCreator.h
@@ -78,7 +78,9 @@ class JoinerTaskCreator: public TaskCreator,  public CorePlugin{
 
 	vector<vector<Kmer> >*m_newPaths;
 
+	// fast run parameters
 	bool m_previouslyDone;
+	bool m_fastRun;
 
 public:
 	void constructor( VirtualProcessor*virtualProcessor,StaticVector*outbox,

code/plugin_MachineHelper/MachineHelper.cpp
@@ -698,7 +698,7 @@ void MachineHelper::call_RAY_MASTER_MODE_START_FUSION_CYCLE(){
 	//  * a distribute cycle
 	//  * a fusion cycle
 
-	int lastAllowedCycleNumber=5;
+	int lastAllowedCycleNumber=2; //5;
 
 	if(!m_cycleStarted){
 		int count=0;

code/plugin_MessageProcessor/MessageProcessor.cpp
@@ -1976,8 +1976,9 @@ void MessageProcessor::call_RAY_MPI_TAG_CLEAR_DIRECTIONS(Message*message){
 
 		if(!eliminated){
 			fusions.push_back((m_ed->m_EXTENSION_contigs)[i]);
-			if(!appendReverseComplement)
+			if(!appendReverseComplement){
 				continue;
+			}
 
 			vector<Kmer> rc;
 			for(int j=(m_ed->m_EXTENSION_contigs)[i].size()-1;j>=0;j--){