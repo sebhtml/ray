d54427b0a9baa0d7a27a28803b13131ab6db1127
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-08-11 18:42:19 -0400

    I added -fwhole-program for better optimization.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- scripts/Build-Link-Time-Optimization.sh
+++ scripts/Build-Link-Time-Optimization.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 mpicxx \
--Wall -ansi -O3 -march=native -flto \
+-Wall -ansi -O3 -march=native -flto -fwhole-program \
  -flto-report \
  -D MAXKMERLENGTH=32  \
  -D RAY_VERSION=\"2.1.0-devel\" -D RAYPLATFORM_VERSION=\"1.1.0-devel\" \
