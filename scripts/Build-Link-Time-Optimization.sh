163eabc0620a3060647714bf7a7cf8f9c42c974a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-08-11 17:59:32 -0400

    I added a script to build Ray with link time optimization.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ scripts/Build-Link-Time-Optimization.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mpicxx \
+-Wall -ansi -O3 -march=native -flto \
+ -flto-report \
+ -D MAXKMERLENGTH=32  \
+ -D RAY_VERSION=\"2.1.0-devel\" -D RAYPLATFORM_VERSION=\"1.1.0-devel\" \
+ -I. -Icode -IRayPlatform \
+ $(find code/|grep .cpp$;find RayPlatform/|grep .cpp$) \
+ -o Ray
