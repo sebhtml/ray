0a6c79c3e7f8d1ef65ec86ccffed6c295ebe6cc3
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-09-19 09:34:54 -0400

    scripts: initial version of a script to create NCBI taxonomy
    Ray Communities can use the NCBI taxonomy to classify k-mers.
    
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ scripts/NCBI-Taxonomy/CreateRayInputStructures.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Author: Sébastien Boisvert
+# Date: 2012-09-19
+
+program=$0
+OutputDirectory=$1
+
+# From: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+command -v wget >/dev/null 2>&1 || { echo >&2 "Error, needs wget but it's not installed.  Aborting."; exit 1; }
+
+
+if test "$OutputDirectory" = ""
+then
+	echo "Error: no output directory was provided, please provide a directory."
+	echo "Usage: $program OutputDirectoryName"
+	exit 1
+fi
+
+if test -d $OutputDirectory
+then
+	echo "Error: the output directory $OutputDirectory already exists, will not overwrite."
+	exit 1
+fi
+
+mkdir $OutputDirectory
+cd $OutputDirectory
+
+if test ! -d ftp.ncbi.nih.gov
+then
+	mkdir ftp.ncbi.nih.gov
+	cd ftp.ncbi.nih.gov
+
+	if test ! -f gi_taxid_nucl.dmp.gz
+	then
+		wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz
+	fi
+
+	if test ! -f taxdump.tar.gz
+	then
+		wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+		cat taxdump.tar.gz|gunzip|tar -x
+	fi
+
+	cd ..
+fi
+
+if test ! -d for-ray
+then
+	mkdir for-ray
+	cd for-ray
+
+	if test ! -d NCBI-Finished-Bacterial-Genomes
+	then
+		mkdir NCBI-Finished-Bacterial-Genomes
+	fi
+
+	if test ! -d NCBI-Draft-Bacterial-Genomes
+	then
+		mkdir NCBI-Draft-Bacterial-Genomes
+	fi
+
+	if test ! -d NCBI-Taxonomy
+	then
+		mkdir NCBI-Taxonomy
+		cd NCBI-Taxonomy
+
+		cat ../../ftp.ncbi.nih.gov/gi_taxid_nucl.dmp.gz|gunzip > Genome-to-Taxon.tsv
+
+		cat ../../ftp.ncbi.nih.gov/nodes.dmp|awk '{print $1"\t"$3}' > TreeOfLife-Edges.tsv
+
+		CreateTaxonNames.py
+
+
+		cd ..
+	fi
+
+	cd ..
+fi
