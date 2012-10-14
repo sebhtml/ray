sebhtml/ray

commit c6f228900b05ef0534e82803eebce65b5ec5b828

Searcher: browsing the distributed colored de Bruijn subgraph
This patch adds code paths for extracting colored summaries
from the distributed de Bruijn subgraph. Forthcoming patches
will implement the code that returns a virtual color from
an array of physical colors.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

code/plugin_Searcher/Searcher.cpp
@@ -38,26 +38,26 @@
 
 __CreatePlugin(Searcher);
 
- /**/
-__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_COUNT_SEARCH_ELEMENTS); /**/
-__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_CONTIG_BIOLOGICAL_ABUNDANCES); /**/
-__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES); /**/
-__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_ADD_COLORS); /**/
-__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_SEARCHER_CLOSE); /**/
- /**/
-__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_COUNT_SEARCH_ELEMENTS); /**/
-__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_CONTIG_BIOLOGICAL_ABUNDANCES); /**/
-__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES); /**/
-__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_ADD_COLORS); /**/
-__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_SEARCHER_CLOSE); /**/
- /**/
-__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_ADD_KMER_COLOR); /**/
-__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_CONTIG_IDENTIFICATION); /**/
-__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS); /**/
-__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_GET_COVERAGE_AND_PATHS); /**/
-__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY); /**/
+__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_COUNT_SEARCH_ELEMENTS);
+__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_CONTIG_BIOLOGICAL_ABUNDANCES);
+__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES);
+__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_ADD_COLORS);
+__CreateMasterModeAdapter(Searcher,RAY_MASTER_MODE_SEARCHER_CLOSE);
+
+__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_COUNT_SEARCH_ELEMENTS);
+__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_CONTIG_BIOLOGICAL_ABUNDANCES);
+__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES);
+__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_ADD_COLORS);
+__CreateSlaveModeAdapter(Searcher,RAY_SLAVE_MODE_SEARCHER_CLOSE);
+
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_ADD_KMER_COLOR);
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_CONTIG_IDENTIFICATION);
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS);
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_GET_COVERAGE_AND_PATHS);
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY);
 __CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_GRAPH_COUNTS);
- /**/
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_VIRTUAL_COLOR_DATA);
+__CreateMessageTagAdapter(Searcher,RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY);
 
 using namespace std;
 
@@ -72,6 +72,21 @@ using namespace std;
 
 #define CONFIG_SEARCH_THRESHOLD 0.001
 #define CONFIG_FORCE_VALUE_FOR_MAXIMUM_SPEED false
+
+int Searcher::getNamespace(PhysicalKmerColor handle){
+
+	return handle/COLOR_NAMESPACE_MULTIPLIER;
+}
+
+PhysicalKmerColor Searcher::buildGlobalHandle(int theNamespace,PhysicalKmerColor handle){
+
+	return handle+theNamespace*COLOR_NAMESPACE_MULTIPLIER;
+}
+
+PhysicalKmerColor Searcher::getColorInNamespace(PhysicalKmerColor handle){
+
+	return handle%COLOR_NAMESPACE_MULTIPLIER;
+}
 
 void Searcher::constructor(Parameters*parameters,StaticVector*outbox,TimePrinter*timePrinter,SwitchMan*switchMan,
 	VirtualCommunicator*vc,StaticVector*inbox,RingAllocator*outboxAllocator,
@@ -551,7 +566,7 @@ void Searcher::countKmerObservations(LargeCount*localAssembledKmerObservations,
 
 			PhysicalKmerColor physicalColor=*j;
 	
-			int nameSpace=physicalColor/COLOR_NAMESPACE_MULTIPLIER;
+			int nameSpace=getNamespace(physicalColor);
 		
 			if(nameSpace==COLOR_NAMESPACE_EMBL_CDS){
 				isGeneForGeneOntologyProfiling=true;
@@ -1302,6 +1317,86 @@ void Searcher::call_RAY_SLAVE_MODE_SEARCHER_CLOSE(){
 	cout<<""<<CONFIG_FILE_IO_BUFFER_SIZE<<" bytes"<<endl;
 }
 
+/*
+ * Browse the graph. Here we send coloring summary to 
+ * master. However, we can not send virtual color counts alone
+ * as the color set is not the same across the processes.
+ * Virtual color handles are 32 bits whereas physical color handles are
+ * 64 bits.
+ */
+void Searcher::browseColoredGraph(){
+	if(!m_browsedTheGraphStarted){
+		
+		m_browsedTheGraphStarted=true;
+		m_currentVirtualColor=0;
+
+		#ifdef ASSERT
+		assert(!m_waiting);
+		#endif /* ASSERT */
+
+		m_messageSent=false;
+		m_messageReceived=false;
+
+	}else if(m_currentVirtualColor<m_colorSet.getTotalNumberOfVirtualColors()){
+
+		if(!m_messageSent){
+/*
+ * Send a message with the virtual color
+ */
+	
+			MessageUnit*messageBuffer=(MessageUnit*)m_outboxAllocator->allocate(MAXIMUM_MESSAGE_SIZE_IN_BYTES);
+			int position=0;
+			messageBuffer[position++]=m_currentVirtualColor;
+			LargeCount references=m_colorSet.getNumberOfReferences(m_currentVirtualColor);
+			messageBuffer[position++]=references;
+			int physicalColors=m_colorSet.getNumberOfPhysicalColors(m_currentVirtualColor);
+
+			int unitsAvailable=MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(MessageUnit)-position;
+
+/*
+ * Browsing the graph with too many physical colors
+ * is not implemented at the moment.
+ * You should therefore use the option -one-color-per-file
+ * .
+ */
+			if(unitsAvailable<physicalColors){
+				physicalColors=0;
+			}
+			messageBuffer[position++]=physicalColors;
+
+			if(physicalColors!=0){
+				set<PhysicalKmerColor>*physicalColors=m_colorSet.getPhysicalColors(m_currentVirtualColor);
+
+				for(set<PhysicalKmerColor>::iterator i=physicalColors->begin();
+					i!=physicalColors->end();++i){
+
+					PhysicalKmerColor handle=*i;
+					messageBuffer[position++]=handle;
+				}
+			}
+			
+
+			Message aMessage(messageBuffer,position,MASTER_RANK,
+				RAY_MPI_TAG_VIRTUAL_COLOR_DATA,m_rank);
+
+			m_outbox->push_back(aMessage);
+
+			m_messageSent=true;
+			m_messageReceived=false;
+
+		}else if(m_messageReceived){
+
+			m_currentVirtualColor++;
+			m_messageSent=false;
+			m_messageReceived=false;
+		}
+
+	}else{
+
+		m_browsedTheGraphEnded=true;
+	}
+}
+
 /** massively parallel implementation of
  * the biological abundance problem solver.
  * each MPI rankcount things.
@@ -1394,7 +1489,7 @@ void Searcher::call_RAY_SLAVE_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES(){
 
 		int slice=total/m_parameters->getSize();
 	
-		int rank=m_parameters->getRank();
+		Rank rank=m_parameters->getRank();
 		m_firstSequence=rank*slice;
 		m_lastSequence=(rank+1)*slice;
 	
@@ -1571,8 +1666,20 @@ void Searcher::call_RAY_SLAVE_MODE_SEQUENCE_BIOLOGICAL_ABUNDANCES(){
 
 	}else if(m_inbox->hasMessage(RAY_MPI_TAG_SEQUENCE_ABUNDANCE_YOU_CAN_GO_HOME)){
 
+		m_browseTheGraph=true;
+		m_browsedTheGraphStarted=false;
+		m_browsedTheGraphEnded=false;
+
+	}else if(m_browseTheGraph && !m_browsedTheGraphEnded){
+
+		browseColoredGraph();
+
+	}else if(m_browsedTheGraphEnded){
+
 		// this kills the batman
 		m_switchMan->closeSlaveModeLocally(m_outbox,m_parameters->getRank());
+
+		m_browsedTheGraphEnded=false;
 
 	// all files in a directory were processed
 	}else if(!m_finished && m_fileIterator==(int)m_searchDirectories[m_directoryIterator].getSize()){
@@ -2618,7 +2725,7 @@ void Searcher::call_RAY_MPI_TAG_GET_COVERAGE_AND_PATHS(Message*message){
 				j!=physicalColors->end();j++){
 				PhysicalKmerColor physicalColor=*j;
 		
-				int nameSpace=physicalColor/COLOR_NAMESPACE_MULTIPLIER;
+				int nameSpace=getNamespace(physicalColor);
 			
 				counts[nameSpace]++;
 				if(counts[nameSpace]>1){
@@ -3126,7 +3233,7 @@ void Searcher::call_RAY_SLAVE_MODE_ADD_COLORS(){
 		}
 
 		// generate a color that includes the namespace
-		m_color= colorInNamespace + COLOR_NAMESPACE_MULTIPLIER * (m_directoryIterator);
+		m_color=buildGlobalHandle(m_directoryIterator,colorInNamespace);
 
 		/* unique identifiers have their own namespace */
 
@@ -3135,17 +3242,13 @@ void Searcher::call_RAY_SLAVE_MODE_ADD_COLORS(){
 		if(m_searchDirectories[m_directoryIterator].hasCurrentSequenceIdentifier()){
 			PhysicalKmerColor theIdentifier=m_searchDirectories[m_directoryIterator].getCurrentSequenceIdentifier();
 
-			PhysicalKmerColor nameSpace=COLOR_NAMESPACE_PHYLOGENY;
-			nameSpace*= COLOR_NAMESPACE_MULTIPLIER;
-			m_identifier=theIdentifier + nameSpace;
+			m_identifier=buildGlobalHandle(COLOR_NAMESPACE_PHYLOGENY,theIdentifier);
 
 		}else if(m_searchDirectories[m_directoryIterator].hasIdentifier_EMBL_CDS()){
 
 			PhysicalKmerColor theIdentifier=m_searchDirectories[m_directoryIterator].getIdentifier_EMBL_CDS();
 
-			PhysicalKmerColor nameSpace=COLOR_NAMESPACE_EMBL_CDS;
-			nameSpace*= COLOR_NAMESPACE_MULTIPLIER;
-			m_identifier=theIdentifier+nameSpace;
+			m_identifier=buildGlobalHandle(COLOR_NAMESPACE_EMBL_CDS,theIdentifier);
 
 		}
 
@@ -3714,7 +3817,63 @@ void Searcher::flushContigIdentificationBuffer(int directoryIterator,bool force)
 
 }
 
+void Searcher::call_RAY_MPI_TAG_VIRTUAL_COLOR_DATA(Message*message){
+	MessageUnit*buffer=(MessageUnit*)message->getBuffer();
+	int count=message->getCount();
+	Rank source=message->getSource();
 
+	int position=0;
+	VirtualKmerColorHandle virtualColor=buffer[position++];
+
+/*
+ * A reference is a pair of k-mers.
+ */
+	LargeCount references=buffer[position++];
+
+	if(references>0){
+		int physicalColors=buffer[position++];
+
+/*
+ * The positions from position up to count-1 are the actual
+ * physical colors.
+ */
+	
+		#define CONFIG_COLORED_GRAPH_DEBUG
+
+		#ifdef CONFIG_COLORED_GRAPH_DEBUG
+		cout<<"[call_RAY_MPI_TAG_VIRTUAL_COLOR_DATA] source: "<<source;
+		cout<<" VirtualColor: "<<virtualColor<<" References: "<<references;
+		cout<<" PhysicalColors: "<<physicalColors;
+
+		set<PhysicalKmerColor> coloredBits;
+	
+		for(int i=0;i<physicalColors;i++){
+			PhysicalKmerColor color=buffer[position+i];
+
+			int theNamespace=getNamespace(color);
+			PhysicalKmerColor directColor=getColorInNamespace(color);
+			cout<<" "<<theNamespace<<":"<<directColor;
+
+			coloredBits.insert(color);
+		}
+	
+		VirtualKmerColorHandle globalColor=m_masterColorSet.findVirtualColor(&coloredBits);
+		for(int i=0;i<references;i++){
+			m_masterColorSet.incrementReferences(globalColor);
+		}
+
+		cout<<endl;
+
+		#endif /* CONFIG_COLORED_GRAPH_DEBUG */
+	}
+
+	m_switchMan->sendEmptyMessage(m_outbox,m_rank,source,RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY);
+}
+
+void Searcher::call_RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY(Message*message){
+
+	m_messageReceived=true;
+}
 
 void Searcher::flushCoverageXMLBuffer(bool force){
 	
@@ -3852,7 +4011,8 @@ void Searcher::registerPlugin(ComputeCore*core){
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY,"RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY");
 
 	RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY_REPLY=core->allocateMessageTagHandle(plugin);
-	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY_REPLY,"RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY_REPLY");
+	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY_REPLY,
+		"RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCE_ENTRY_REPLY");
 
 	RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCES=core->allocateMessageTagHandle(plugin);
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCES,"RAY_MPI_TAG_WRITE_SEQUENCE_ABUNDANCES");
@@ -3864,15 +4024,30 @@ void Searcher::registerPlugin(ComputeCore*core){
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_ADD_KMER_COLOR_REPLY,"RAY_MPI_TAG_ADD_KMER_COLOR_REPLY");
 
 	RAY_MPI_TAG_CONTIG_IDENTIFICATION=core->allocateMessageTagHandle(plugin);
-	core->setMessageTagObjectHandler(plugin,RAY_MPI_TAG_CONTIG_IDENTIFICATION,__GetAdapter(Searcher,RAY_MPI_TAG_CONTIG_IDENTIFICATION));
+	core->setMessageTagObjectHandler(plugin,RAY_MPI_TAG_CONTIG_IDENTIFICATION,
+		__GetAdapter(Searcher,RAY_MPI_TAG_CONTIG_IDENTIFICATION));
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_CONTIG_IDENTIFICATION,"RAY_MPI_TAG_CONTIG_IDENTIFICATION");
 
 	RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS=core->allocateMessageTagHandle(m_plugin);
-	core->setMessageTagObjectHandler(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS,__GetAdapter(Searcher,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS));
-	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS,"RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS");
+	core->setMessageTagObjectHandler(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS,
+		__GetAdapter(Searcher,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS));
+	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS,
+		"RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS");
 	
+	RAY_MPI_TAG_VIRTUAL_COLOR_DATA=core->allocateMessageTagHandle(m_plugin);
+	core->setMessageTagObjectHandler(m_plugin,RAY_MPI_TAG_VIRTUAL_COLOR_DATA,
+		__GetAdapter(Searcher,RAY_MPI_TAG_VIRTUAL_COLOR_DATA));
+	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_VIRTUAL_COLOR_DATA,"RAY_MPI_TAG_VIRTUAL_COLOR_DATA");
+
+	RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY=core->allocateMessageTagHandle(m_plugin);
+	core->setMessageTagObjectHandler(m_plugin,RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY,
+		__GetAdapter(Searcher,RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY));
+	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY,
+		"RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY");
+
 	RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS_REPLY=core->allocateMessageTagHandle(m_plugin);
-	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS_REPLY,"RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS_REPLY");
+	core->setMessageTagSymbol(m_plugin,RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS_REPLY,
+		"RAY_MPI_TAG_REQUEST_VERTEX_COVERAGE_AND_COLORS_REPLY");
 
 	RAY_MPI_TAG_CONTIG_IDENTIFICATION_REPLY=core->allocateMessageTagHandle(plugin);
 	core->setMessageTagSymbol(plugin,RAY_MPI_TAG_CONTIG_IDENTIFICATION_REPLY,"RAY_MPI_TAG_CONTIG_IDENTIFICATION_REPLY");
@@ -3883,7 +4058,6 @@ void Searcher::registerPlugin(ComputeCore*core){
 	core->setObjectSymbol(m_plugin,m_timePrinter,"/RayAssembler/ObjectStore/Timer.ray");
 	core->setObjectSymbol(m_plugin,this,"/RayAssembler/ObjectStore/plugin_Searcher.ray");
 	core->setObjectSymbol(m_plugin,&m_contigLengths,"/RayAssembler/ObjectStore/ContigLengths.ray");
-
 
 	m_totalNumberOfColoredKmerObservations=0;
 	m_totalNumberOfColoredKmers=0;
@@ -3994,6 +4168,11 @@ void Searcher::resolveSymbols(ComputeCore*core){
 	m_coverageXMLflushOperations=0;
 	m_contigIdentificationflushOperations=0;
 	m_sequenceXMLflushOperations=0;
+
+	m_browseTheGraph=false;
+	m_browsedTheGraphStarted=false;
+	m_browsedTheGraphEnded=false;
+	m_rank=m_parameters->getRank();
 
 	__BindPlugin(Searcher);
 }

code/plugin_MessageProcessor/MessageProcessor.cpp
@@ -718,6 +718,15 @@ void MessageProcessor::call_RAY_MPI_TAG_VERTICES_DATA(Message*message){
 		Kmer kmerObject;
 		int pos=i;
 		kmerObject.unpack(incoming,&pos);
+
+/* make sure that the payload 
+ * is for this process and not another one...
+ */
+		#ifdef ASSERT
+		Rank rankToFlush=kmerObject.hash_function_1()%m_parameters->getSize();
+		assert(rankToFlush==m_rank);
+		#endif
+
 		//bool isTheLowerKmer=false;
 		Kmer lowerKmer=kmerObject;
 
@@ -780,6 +789,11 @@ void MessageProcessor::call_RAY_MPI_TAG_VERTICES_DATA(Message*message){
 		assert(tmp!=NULL);
 		#endif
 
+/*
+ * Initialize the k-mer coverage 
+ * It starts at 0 if the Bloom filter
+ * is disabled, 1 otherwise.
+ */
 		if(m_subgraph->inserted()){
 			tmp->constructor(); 
 		

code/plugin_Searcher/ColorSet.cpp
@@ -29,8 +29,11 @@
 #include <iostream>
 using namespace std;
 
+#ifdef ASSERT
 #include <assert.h>
+#endif /* ASSERT */
 
+#define NULL_VIRTUAL_COLOR 0
 
 /** Time complexity: constant **/
 ColorSet::ColorSet(){
@@ -72,7 +75,7 @@ void ColorSet::incrementReferences(VirtualKmerColorHandle handle){
 	#endif
 
 	// we don't care about the empty virtual color
-	if(handle == 0)
+	if(handle == NULL_VIRTUAL_COLOR)
 		return;
 
 	VirtualKmerColor*virtualColor=getVirtualColor(handle);
@@ -101,7 +104,7 @@ void ColorSet::decrementReferences(VirtualKmerColorHandle handle){
 	VirtualKmerColor*virtualColor=getVirtualColor(handle);
 
 	// we don't care aboput the empty virtual color
-	if(handle == 0)
+	if(handle == NULL_VIRTUAL_COLOR)
 		return;
 
 	#ifdef ASSERT
@@ -113,7 +116,7 @@ void ColorSet::decrementReferences(VirtualKmerColorHandle handle){
 
 	// destroy the color if it is not used anymore
 	// and if it is not the color 0
-	if(handle>0 && virtualColor->getNumberOfReferences()==0){
+	if(handle!=NULL_VIRTUAL_COLOR && virtualColor->getNumberOfReferences()==0){
 		purgeVirtualColor(handle);
 	}
 
@@ -364,7 +367,7 @@ uint64_t ColorSet::applyHashOperation(uint64_t hashValue,PhysicalKmerColor color
 	return hashValue;
 }
 
-int ColorSet::getNumberOfReferences(VirtualKmerColorHandle handle){
+LargeCount ColorSet::getNumberOfReferences(VirtualKmerColorHandle handle){
 	#ifdef ASSERT
 	assert(handle< getTotalNumberOfVirtualColors());
 	#endif
@@ -392,7 +395,6 @@ VirtualKmerColorHandle ColorSet::getVirtualColorFrom(VirtualKmerColorHandle hand
 	m_operations[OPERATION_getVirtualColorFrom]++;
 
 	m_physicalColors.insert(color);
-
 
 	VirtualKmerColor*oldVirtualColor=getVirtualColor(handle);
 	// check if any of them have the correct hash
@@ -444,7 +446,7 @@ VirtualKmerColorHandle ColorSet::getVirtualColorFrom(VirtualKmerColorHandle hand
 
 		#ifdef ASSERT_LOW_LEVEL
 		assertNoVirtualColorDuplicates(handle,color,433);
-		#endif
+		#endif /* ASSERT_LOW_LEVEL */
 	
 		return createVirtualColorFrom(handle,color);
 	}
@@ -452,7 +454,7 @@ VirtualKmerColorHandle ColorSet::getVirtualColorFrom(VirtualKmerColorHandle hand
 	#ifdef ASSERT
 	assert(m_index.count(expectedHash)>0);
 	assert(m_index[expectedHash].size()>=1);
-	#endif
+	#endif /* ASSERT */
 
 	// for each of the hits
 	// check if they have all the required colors
@@ -479,7 +481,7 @@ VirtualKmerColorHandle ColorSet::getVirtualColorFrom(VirtualKmerColorHandle hand
 
 			#ifdef ASSERT
 			assert(virtualColorHasPhysicalColor(virtualColorToInvestigate,color));
-			#endif
+			#endif /* ASSERT */
 	
 			m_operations[OPERATION_VIRTUAL_COLOR_HAS_COLORS_FETCH]++;
 			return virtualColorToInvestigate;
@@ -511,7 +513,7 @@ VirtualKmerColorHandle ColorSet::createVirtualColorFrom(VirtualKmerColorHandle h
 	assert(m_availableHandles.count(newHandle)==0);
 	assert(getNumberOfReferences(newHandle)==0);
 	assert(getNumberOfPhysicalColors(newHandle)==0);
-	#endif
+	#endif /* ASSERT */
 
 	// add the colors and return it
 	VirtualKmerColor*newVirtualColor=getVirtualColor(newHandle);
@@ -614,3 +616,8 @@ void ColorSet::printPhysicalColors(set<PhysicalKmerColor>*colors3){
 	}
 	cout<<endl;
 }
+
+VirtualKmerColorHandle ColorSet::findVirtualColor(set<PhysicalKmerColor>*colors){
+	return NULL_VIRTUAL_COLOR;
+}
+

code/plugin_Searcher/ColorSet.h
@@ -59,11 +59,11 @@ class ColorSet{
 	int OPERATION_NO_VIRTUAL_COLOR_HAS_HASH_CREATION;
 	int OPERATION_VIRTUAL_COLOR_HAS_COLORS_FETCH;
 	int OPERATION_NO_VIRTUAL_COLOR_HAS_COLORS_CREATION;
-	int  OPERATION_NEW_FROM_EMPTY;
+	int OPERATION_NEW_FROM_EMPTY;
 	int OPERATION_NEW_FROM_SCRATCH;
-	int  OPERATION_applyHashOperation;
+	int OPERATION_applyHashOperation;
 	int OPERATION_getHash;
-	int  OPERATION_getVirtualColorFrom;
+	int OPERATION_getVirtualColorFrom;
 	int OPERATION_createVirtualColorFrom ;
 	int OPERATION_incrementReferences ;
 	int OPERATION_decrementReferences ;
@@ -96,8 +96,6 @@ class ColorSet{
 
 	VirtualKmerColor*getVirtualColor(VirtualKmerColorHandle handle);
 
-/** get a handle to a virtual color */
-	VirtualKmerColorHandle getVirtualColorHandle(set<PhysicalKmerColor>*colors);
 
 	void purgeVirtualColor(VirtualKmerColorHandle handle);
 
@@ -125,7 +123,7 @@ public:
 /** decrement the references for a virtual color **/
 	void decrementReferences(VirtualKmerColorHandle handle);
 	
-	int getNumberOfReferences(VirtualKmerColorHandle handle);
+	LargeCount getNumberOfReferences(VirtualKmerColorHandle handle);
 
 	int getNumberOfPhysicalColors(VirtualKmerColorHandle handle);
 
@@ -140,6 +138,11 @@ public:
 	set<PhysicalKmerColor>*getPhysicalColors(VirtualKmerColorHandle handle);
 
 	bool virtualColorHasPhysicalColor(VirtualKmerColorHandle handle,PhysicalKmerColor color);
+
+/*
+ * Returns a virtual color with exactly the physical colors provided.
+ */
+	VirtualKmerColorHandle findVirtualColor(set<PhysicalKmerColor>*colors);
 };
 
 #endif

code/plugin_Searcher/Searcher.h
@@ -65,6 +65,16 @@ class Searcher :  public CorePlugin {
 /* the number of colored k-mers for a contig */
 	int m_coloredKmers;
 
+/* states for browsing the graph */
+	bool m_browseTheGraph;
+	bool m_browsedTheGraphStarted;
+	bool m_browsedTheGraphEnded;
+	VirtualKmerColorHandle m_currentVirtualColor;
+	Rank m_rank;
+	bool m_messageSent;
+	bool m_messageReceived;
+	
+
 	QualityCaller m_caller;
 
 	MessageTag RAY_MPI_TAG_ADD_COLORS;
@@ -103,6 +113,8 @@ class Searcher :  public CorePlugin {
 	MessageTag RAY_MPI_TAG_GET_GRAPH_COUNTS;
 	MessageTag RAY_MPI_TAG_GET_GRAPH_COUNTS_REPLY;
 	MessageTag RAY_MPI_TAG_GRAPH_COUNTS;
+	MessageTag RAY_MPI_TAG_VIRTUAL_COLOR_DATA;
+	MessageTag RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY;
 
 	MasterMode RAY_MASTER_MODE_KILL_RANKS;
 	MasterMode RAY_MASTER_MODE_ADD_COLORS;
@@ -123,6 +135,11 @@ class Searcher :  public CorePlugin {
 
 /** translator for virtual colors **/
 	ColorSet m_colorSet;
+
+/*
+ * The master color object
+ */
+	ColorSet m_masterColorSet;
 
 	int m_finishedColoring;
 	bool m_locallyFinishedColoring;
@@ -387,6 +404,11 @@ class Searcher :  public CorePlugin {
 	// EMBL_CDS namespace
 	LargeCount m_totalNumberOfKmerObservations_EMBL_CDS;
 
+	void browseColoredGraph();
+	PhysicalKmerColor getColorInNamespace(PhysicalKmerColor handle);
+	PhysicalKmerColor buildGlobalHandle(int theNamespace,PhysicalKmerColor handle);
+	int getNamespace(PhysicalKmerColor handle);
+
 public:
 
 	void call_RAY_MASTER_MODE_COUNT_SEARCH_ELEMENTS();
@@ -425,8 +447,11 @@ public:
 
 	LargeCount getTotalNumberOfColoredKmerObservationsForANameSpace(int namespaceNumber);
 
+	void call_RAY_MPI_TAG_VIRTUAL_COLOR_DATA(Message*message);
+	void call_RAY_MPI_TAG_VIRTUAL_COLOR_DATA_REPLY(Message*message);
+
 }; /* Searcher */
 
 
-#endif
+#endif /* _Searcher_h */
 

code/plugin_Searcher/VirtualKmerColor.h
@@ -42,10 +42,14 @@ typedef uint64_t PhysicalKmerColor;
  */
 class VirtualKmerColor{
 
-/** half the number of k-mers using this virtual color **/
+/** 
+ * half the number of k-mers using this virtual color 
+ */
 	LargeCount m_references;
 
-/** the list of physical colors **/
+/**
+ * the list of physical colors 
+ */
 	set<PhysicalKmerColor> m_colors;
 
 	uint64_t m_hash;

code/plugin_VerticesExtractor/Vertex.h
@@ -50,6 +50,15 @@ using namespace std;
  */
 class Vertex{
 	
+/**
+ * This is for the colored graph subsystem.
+ * Right now, this subsystem stores only the origins
+ * of a k-mer, not an array of coverage depths.
+ * Therefore, this attribute is not required
+ * as the number of references is tracked elsewhere
+ * anyway.
+ * TODO: remove m_color because the references are tracked elsewhere too 
+ */
 	VirtualKmerColorHandle m_color;
 
 

code/plugin_VerticesExtractor/VerticesExtractor.cpp
@@ -231,6 +231,7 @@ void VerticesExtractor::call_RAY_SLAVE_MODE_ADD_EDGES(){
 
 
 				if(m_bufferedDataForOutgoingEdges.flush(outgoingRank,2*KMER_U64_ARRAY_SIZE,RAY_MPI_TAG_OUT_EDGES_DATA,m_outboxAllocator,m_outbox,m_parameters->getRank(),false)){
+
 					m_pendingMessages++;
 				}
 

commit beb565ad8750d0bfb4c5eaab1bb8a93ec52b209a

Searcher: fixed buffer overflow
This commit fixes a regression introduced by commit
c6f228900b05ef0534e82803eebce65b5ec5b828. The number of available
units must consider also the number of physical colors.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

code/plugin_Searcher/Searcher.cpp
@@ -1363,18 +1363,24 @@ void Searcher::browseColoredGraph(){
 			messageBuffer[position++]=references;
 			int physicalColors=m_colorSet.getNumberOfPhysicalColors(m_currentVirtualColor);
 
-			int unitsAvailable=MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(MessageUnit)-position;
+			int positionForPhysicalColors=position++;
+/**
+ * 3 units must be removed, not 2. This is because we need to store
+ * the virtual color, its number of references, and its
+ * number of physical colors.
+ */
+			int unitsAvailable=(MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(MessageUnit))-position;
 
 /*
  * Browsing the graph with too many physical colors
  * is not implemented at the moment.
- * You should therefore use the option -one-color-per-file
- * .
+ * You should therefore use the option -one-color-per-file.
  */
 			if(unitsAvailable<physicalColors){
 				physicalColors=0;
 			}
-			messageBuffer[position++]=physicalColors;
+
+			messageBuffer[positionForPhysicalColors]=physicalColors;
 
 			if(physicalColors!=0){
 				set<PhysicalKmerColor>*physicalColors=m_colorSet.getPhysicalColors(m_currentVirtualColor);
@@ -1386,6 +1392,10 @@ void Searcher::browseColoredGraph(){
 					messageBuffer[position++]=handle;
 				}
 			}
+
+			#ifdef ASSERT
+			assert(position<=MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(MessageUnit));
+			#endif /* ASSERT */
 
 			Message aMessage(messageBuffer,position,MASTER_RANK,
 				RAY_MPI_TAG_VIRTUAL_COLOR_DATA,m_rank);