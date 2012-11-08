2ba4d5020eb9cb4f68ec4504843b8d34613aa637
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-11-08 14:12:34 -0500

    ship: removed 6 files in shipped products
    In order to be able to generate tarballs just from
    git directly, I removed 6 generated files containing
    git tag, and git addresses.
    
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- scripts/ShipProduct.sh
+++ scripts/ShipProduct.sh
@@ -6,21 +6,7 @@ echo "Welcome to Ray Technologies."
 echo "Today we ship a product."
 echo ""
 
-# you need 
-
-# - git
-# - mkdir (coreutils)
-# - bash
-# - xz
-# - tar
-# - gzip
-# - bzip2
-# - zip
-# - md5sum
-# - sha1sum
-
 release=$1
-
 base=~/git-clones
 
 RayGit=$base/ray
@@ -39,37 +25,21 @@ echo "Assembling product parts"
 mkdir -p $productDirectory
 cd $productDirectory
 
-echo "Cloning $RayGit"
 git clone $RayGit $productName &> /dev/null
 cd $productName
-git log|grep ^commit|head -n1|awk '{print $2}' > tag.txt
-echo https://github.com/sebhtml/ray > github.txt
-echo git://github.com/sebhtml/ray.git > git.txt
 rm -rf .git
 rm -rf RayPlatform
 
-echo "Cloning $RayPlatformGit"
 git clone $RayPlatformGit &> /dev/null
 cd RayPlatform
-git log|grep ^commit|head -n1|awk '{print $2}' > tag.txt
-echo https://github.com/sebhtml/RayPlatform > github.txt
-echo git://github.com/sebhtml/RayPlatform.git > git.txt
 rm -rf .git
 cd ..
-
-
-
-# create archives
+cd ..
 
 echo "Creating product releases."
-
-cd ..
-echo ".tar.bz2"
-tar cjf $productName.tar.bz2 $productName &> /dev/null
+tar -cjf $productName.tar.bz2 $productName &> /dev/null
 
 rm -rf $productName
-
-# compute checksums
 
 for i in $(ls Ray-$release.*)
 do
