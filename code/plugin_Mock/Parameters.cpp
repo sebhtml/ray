sebhtml/ray

commit 42100299b3fa513c65c72fd685530c956ac4437e

Fixed the scope of options in build system
HAVE_LIBZ and HAVE_LIBBZ2 are the names for the end user.
Internally, the Makefiles and C++ code files use
CONFIG_HAVE_LIBZ and CONFIG_LIBBZ2. Therefore, these
must be exported because a user may edit the Makefile
manually.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

code/plugin_SequencesLoader/Loader.cpp
@@ -92,7 +92,7 @@ int Loader::load(string file,bool isGenome){
 		return ret;
 	}
 
-	#ifdef HAVE_LIBZ
+	#ifdef CONFIG_HAVE_LIBZ
 	if(file.substr(file.length()-9,9)==".fastq.gz"){
 		m_type=FORMAT_FASTQ_GZ;
 		int ret=m_fastqgz.open(file,4);
@@ -168,7 +168,7 @@ void Loader::loadSequences(){
 	m_reads.reset();
 
 	if(m_type==FORMAT_FASTQ_GZ){
-		#ifdef HAVE_LIBZ
+		#ifdef CONFIG_HAVE_LIBZ
 		m_fastqgz.load(m_maxToLoad,&m_reads,&m_allocator,4);
 		#endif
 	}else if(m_type==FORMAT_FASTQ){
@@ -188,7 +188,7 @@ void Loader::loadSequences(){
 		m_fastqbz2.load(m_maxToLoad,&m_reads,&m_allocator,2);
 		#endif
 	}else if(m_type==FORMAT_FASTA_GZ){
-		#ifdef HAVE_LIBZ
+		#ifdef CONFIG_HAVE_LIBZ
 		m_fastqgz.load(m_maxToLoad,&m_reads,&m_allocator,2);
 		#endif
 	}

code/plugin_SequencesLoader/Loader.h
@@ -30,7 +30,7 @@
 #include <plugin_SequencesLoader/ArrayOfReads.h>
 #include <string>
 
-#ifdef HAVE_LIBZ
+#ifdef CONFIG_HAVE_LIBZ
 #include<plugin_SequencesLoader/FastqGzLoader.h>
 #endif
 
@@ -78,7 +78,7 @@ class Loader{
 	FastqLoader m_fastq;	
 	FastaLoader m_fasta;	
 
-	#ifdef HAVE_LIBZ
+	#ifdef CONFIG_HAVE_LIBZ
 	FastqGzLoader m_fastqgz;
 	#endif
 

code/application_core/Parameters.cpp
@@ -841,13 +841,13 @@ void Parameters::parseCommands(){
 			if(m_wordSize<15){
 				m_wordSize=15;
 			}
-			if(m_wordSize>MAXKMERLENGTH){
+			if(m_wordSize>CONFIG_MAXKMERLENGTH){
 				if(m_rank==MASTER_RANK){
 					cout<<endl;
-					cout<<"Rank "<<MASTER_RANK<<": Warning, k > MAXKMERLENGTH"<<endl;
-					cout<<"Rank "<<MASTER_RANK<<": Change MAXKMERLENGTH in the Makefile and recompile Ray."<<endl;
+					cout<<"Rank "<<MASTER_RANK<<": Warning, k > CONFIG_MAXKMERLENGTH"<<endl;
+					cout<<"Rank "<<MASTER_RANK<<": Change CONFIG_MAXKMERLENGTH in the Makefile and recompile Ray."<<endl;
 				}
-				m_wordSize=MAXKMERLENGTH;
+				m_wordSize=CONFIG_MAXKMERLENGTH;
 			}
 
 			if(m_wordSize%2==0){
@@ -1352,7 +1352,7 @@ void Parameters::showUsage(){
 	cout<<endl;
 	showOption("-k kmerLength","Selects the length of k-mers. The default value is 21. ");
 	showOptionDescription("It must be odd because reverse-complement vertices are stored together.");
-	showOptionDescription("The maximum length is defined at compilation by MAXKMERLENGTH");
+	showOptionDescription("The maximum length is defined at compilation by CONFIG_MAXKMERLENGTH");
 	showOptionDescription("Larger k-mers utilise more memory.");
 	cout<<endl;
 
@@ -1615,10 +1615,10 @@ void Parameters::showUsage(){
 	cout<<"     Note: file format is determined with file extension."<<endl;
 	cout<<endl;
 	cout<<"     .fasta"<<endl;
-	cout<<"     .fasta.gz (needs HAVE_LIBZ=y at compilation)"<<endl;
+	cout<<"     .fasta.gz (needs CONFIG_HAVE_LIBZ=y at compilation)"<<endl;
 	cout<<"     .fasta.bz2 (needs HAVE_LIBBZ2=y at compilation)"<<endl;
 	cout<<"     .fastq"<<endl;
-	cout<<"     .fastq.gz (needs HAVE_LIBZ=y at compilation)"<<endl;
+	cout<<"     .fastq.gz (needs CONFIG_HAVE_LIBZ=y at compilation)"<<endl;
 	cout<<"     .fastq.bz2 (needs HAVE_LIBBZ2=y at compilation)"<<endl;
 	cout<<"     .sff (paired reads must be extracted manually)"<<endl;
 	cout<<"     .csfasta (color-space reads)"<<endl;

Makefile
@@ -4,15 +4,20 @@ SUBLEVEL = 1
 EXTRAVERSION = -devel
 NAME = Ancient Granularity of Epochs
 
-# number of cores to use for compilation
-J=1
-
-RAY_VERSION = $(VERSION).$(PATCHLEVEL).$(SUBLEVEL)$(EXTRAVERSION)
+# like in Linux, stuff that must be propagated to
+# the source code (C++ here) or other Makefiles
+# are prefixed with CONFIG_
 
 # author: Sébastien Boisvert
 # Makefile for the ray assembler
 # Objects appended to obj-y are compiled and linked.
 # Objects appended to obj-n are not compiled and linked.
+
+# number of cores to use for compilation
+J=1
+
+RAY_VERSION = $(VERSION).$(PATCHLEVEL).$(SUBLEVEL)$(EXTRAVERSION)
+
 #############################################
 # compilation options
 
@@ -33,13 +38,13 @@ MAXKMERLENGTH = 32
 # needs libz
 # set to no if you don't have libz
 # y/n
-HAVE_LIBZ = n
+HAVE_LIBZ = y
 
 # support for .bz2 files
 # needs libbz2
 # set to no if you don't have libbz2
 # y/n
-HAVE_LIBBZ2 = n
+HAVE_LIBBZ2 = y
 
 # use Intel's compiler
 # the name of the Intel MPI C++ compiler is mpiicpc
@@ -66,11 +71,11 @@ ASSERT = n
 
 # collect profiling information with -run-profiler
 # if set to n, the code is not even compiled in
-CONFIG_PROFILER_COLLECT=n
+PROFILER_COLLECT=n
 
 # use the precision clock
 # needs -l rt too
-CONFIG_CLOCK_GETTIME=n
+CLOCK_GETTIME=n
 
 # OS detection based on git Makefile
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
@@ -116,24 +121,24 @@ endif
 CXXFLAGS-$(GPROF) += -g -pg
 LDFLAGS-$(GPROF) += -pg -g
 
-# if you use Intel's mpiicpc, uncomment the following lines
+# if you use Intel's mpiicpc
 MPICXX-$(INTEL_COMPILER) = mpiicpc 
-CXXFLAGS-$(INTEL_COMPILER) += -DMPICH_IGNORE_CXX_SEEK -DMPICH_SKIP_MPICXX
+CXXFLAGS-$(INTEL_COMPILER) += -D MPICH_IGNORE_CXX_SEEK -D MPICH_SKIP_MPICXX  # this should be fixed upstream.
 
 #maximum k-mer length
-CXXFLAGS-y += -D MAXKMERLENGTH=$(MAXKMERLENGTH)
+CXXFLAGS-y += -D CONFIG_MAXKMERLENGTH=$(MAXKMERLENGTH)
 CXXFLAGS-y += $(EXTRA)
 LDFLAGS-y += $(EXTRA)
 
 # compile assertions
-CXXFLAGS-$(ASSERT) += -DASSERT
+CXXFLAGS-$(ASSERT) += -D CONFIG_ASSERT -D ASSERT
 
 #compile with libz
-CXXFLAGS-$(HAVE_LIBZ) += -DHAVE_LIBZ
+CXXFLAGS-$(HAVE_LIBZ) += -D CONFIG_HAVE_LIBZ
 LDFLAGS-$(HAVE_LIBZ) += -lz
 
 #compile with libbz2
-CXXFLAGS-$(HAVE_LIBBZ2) += -DHAVE_LIBBZ2 
+CXXFLAGS-$(HAVE_LIBBZ2) += -D CONFIG_HAVE_LIBBZ2 
 LDFLAGS-$(HAVE_LIBBZ2) += -lbz2
 
 #debug flag
@@ -141,11 +146,11 @@ CXXFLAGS-$(DEBUG) += -g
 LDFLAGS-$(DEBUG)  += -g
 
 # pack data in memory to save space
-CXXFLAGS-$(FORCE_PACKING) += -DFORCE_PACKING
+CXXFLAGS-$(FORCE_PACKING) += -D CONFIG_FORCE_PACKING
 
-CXXFLAGS-$(CONFIG_PROFILER_COLLECT) += -D CONFIG_PROFILER_COLLECT
-CXXFLAGS-$(CONFIG_CLOCK_GETTIME) += -D CONFIG_CLOCK_GETTIME
-LDFLAGS-$(CONFIG_CLOCK_GETTIME) += -l rt
+CXXFLAGS-$(PROFILER_COLLECT) += -D CONFIG_PROFILER_COLLECT
+CXXFLAGS-$(CLOCK_GETTIME) += -D CONFIG_CLOCK_GETTIME
+LDFLAGS-$(CLOCK_GETTIME) += -l rt
 CXXFLAGS-y += -D RAY_VERSION=\\\"$(RAY_VERSION)\\\"
 
 CXXFLAGS += $(CXXFLAGS-y)
@@ -153,7 +158,10 @@ LDFLAGS += $(LDFLAGS-y)
 
 MPICXX = $(MPICXX-y)
 
-# object files
+# export some configuration for child Makefile files
+
+export CONFIG_HAVE_LIBZ=$(HAVE_LIBZ)
+export CONFIG_HAVE_LIBBZ2=$(HAVE_LIBBZ2)
 
 #################################
 

code/application_core/Machine.cpp
@@ -609,8 +609,7 @@ void Machine::showRayVersionShort(){
 	CoverageDepth maximumCoverageDepth=0;
 	maximumCoverageDepth--; // underflow it
 
-	cout<<"MAXKMERLENGTH: "<<MAXKMERLENGTH<<endl;
-	cout<<"KMER_U64_ARRAY_SIZE: "<<KMER_U64_ARRAY_SIZE<<endl;
+	cout<<"MAXKMERLENGTH: "<<CONFIG_MAXKMERLENGTH<<endl;
 	cout<<"Maximum coverage depth stored by CoverageDepth: "<<maximumCoverageDepth<<endl;
 	cout<<"MAXIMUM_MESSAGE_SIZE_IN_BYTES: "<<MAXIMUM_MESSAGE_SIZE_IN_BYTES<<" bytes"<<endl;
 
@@ -627,7 +626,7 @@ content
 list
  FORCE_PACKING 
  ASSERT 
- HAVE_LIBZ 
+ CONFIG_HAVE_LIBZ 
  HAVE_LIBBZ2 
  CONFIG_PROFILER_COLLECT 
  CONFIG_CLOCK_GETTIME 
@@ -643,7 +642,7 @@ for i in $(cat list ); do exp="s/option/$i/g"; sed $exp content; done > list2
 	/* generated code */
 
 	cout<<"FORCE_PACKING = ";
-	#ifdef FORCE_PACKING
+	#ifdef CONFIG_FORCE_PACKING
 	cout<<"y";
 	#else
 	cout<<"n";
@@ -659,7 +658,7 @@ for i in $(cat list ); do exp="s/option/$i/g"; sed $exp content; done > list2
 	cout<<endl;
 
 	cout<<"HAVE_LIBZ = ";
-	#ifdef HAVE_LIBZ
+	#ifdef CONFIG_HAVE_LIBZ
 	cout<<"y";
 	#else
 	cout<<"n";
@@ -667,7 +666,7 @@ for i in $(cat list ); do exp="s/option/$i/g"; sed $exp content; done > list2
 	cout<<endl;
 
 	cout<<"HAVE_LIBBZ2 = ";
-	#ifdef HAVE_LIBBZ2
+	#ifdef CONFIG_HAVE_LIBBZ2
 	cout<<"y";
 	#else
 	cout<<"n";

code/application_core/common_functions.cpp
@@ -79,7 +79,7 @@ string convertToString(vector<Kmer>*b,int m_wordSize,bool color){
 
 Kmer kmerAtPosition(const char*m_sequence,int pos,int w,char strand,bool color){
 	#ifdef ASSERT
-	assert(w<=MAXKMERLENGTH);
+	assert(w<=CONFIG_MAXKMERLENGTH);
 	#endif
 	int length=strlen(m_sequence);
 	if(pos>length-w){
@@ -91,13 +91,13 @@ Kmer kmerAtPosition(const char*m_sequence,int pos,int w,char strand,bool color){
 		exit(0);
 	}
 	if(strand=='F'){
-		char sequence[MAXKMERLENGTH];
+		char sequence[CONFIG_MAXKMERLENGTH];
 		memcpy(sequence,m_sequence+pos,w);
 		sequence[w]='\0';
 		Kmer v=wordId(sequence);
 		return v;
 	}else if(strand=='R'){
-		char sequence[MAXKMERLENGTH];
+		char sequence[CONFIG_MAXKMERLENGTH];
 		memcpy(sequence,m_sequence+length-pos-w,w);
 		sequence[w]='\0';
 		Kmer v=wordId(sequence);

code/application_core/constants.h
@@ -1,22 +1,21 @@
 /*
-Ray
-Copyright (C)  2011  Sébastien Boisvert
+ 	Ray
+    Copyright (C) 2011, 2012 Sébastien Boisvert
 
-http://DeNovoAssembler.SourceForge.Net/
+	http://DeNovoAssembler.SourceForge.Net/
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You have received a copy of the GNU General Public License
-along with this program (gpl-3.0.txt).  
-see <http://www.gnu.org/licenses/>
-
+    You have received a copy of the GNU General Public License
+    along with this program (gpl-3.0.txt).  
+	see <http://www.gnu.org/licenses/>
 */
 
 #ifndef _constants
@@ -30,8 +29,8 @@ see <http://www.gnu.org/licenses/>
  * Define the maximum k-mer length when
  * the compiler/make does not.
  */
-#ifndef MAXKMERLENGTH
-#define MAXKMERLENGTH 32
+#ifndef CONFIG_MAXKMERLENGTH
+#define CONFIG_MAXKMERLENGTH 32
 #endif
 
 // some multipliers
@@ -57,7 +56,7 @@ see <http://www.gnu.org/licenses/>
 #define __func__ __FUNCTION__ 
 #endif
 
-#ifdef FORCE_PACKING
+#ifdef CONFIG_FORCE_PACKING
 /*
  * With gcc, one can pack data structures.
  */

code/plugin_KmerAcademyBuilder/Kmer.h
@@ -38,9 +38,9 @@ using namespace std;
 
 /*
  * Determine the number of uint64_t necessary to host 
- * k-mers of maximum length MAXKMERLENGTH
+ * k-mers of maximum length CONFIG_MAXKMERLENGTH
  */
-#define KMER_REQUIRED_BITS (2*MAXKMERLENGTH)
+#define KMER_REQUIRED_BITS (2*CONFIG_MAXKMERLENGTH)
 #define KMER_REQUIRED_BYTES (KMER_REQUIRED_BITS/8)
 #define KMER_REQUIRED_BYTES_MODULO (KMER_REQUIRED_BITS%8)
 #if KMER_REQUIRED_BYTES_MODULO

commit 9cbf2a8277a950743ae651fb6bc85bf653120bd2

SequencesLoader: a regression for .bz2 file support was fixed
The commit 42100299b3fa513c65c72fd685530c956ac4437e introduced a
regression that removed .bz2 file support. This was corrected
herein.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

code/plugin_Mock/Parameters.cpp
@@ -1639,10 +1639,10 @@ void Parameters::showUsage(){
 	cout<<"     Note: file format is determined with file extension."<<endl;
 	cout<<endl;
 	cout<<"     .fasta"<<endl;
-	cout<<"     .fasta.gz (needs CONFIG_HAVE_LIBZ=y at compilation)"<<endl;
+	cout<<"     .fasta.gz (needs HAVE_LIBZ=y at compilation)"<<endl;
 	cout<<"     .fasta.bz2 (needs HAVE_LIBBZ2=y at compilation)"<<endl;
 	cout<<"     .fastq"<<endl;
-	cout<<"     .fastq.gz (needs CONFIG_HAVE_LIBZ=y at compilation)"<<endl;
+	cout<<"     .fastq.gz (needs HAVE_LIBZ=y at compilation)"<<endl;
 	cout<<"     .fastq.bz2 (needs HAVE_LIBBZ2=y at compilation)"<<endl;
 	cout<<"     .sff (paired reads must be extracted manually)"<<endl;
 	cout<<"     .csfasta (color-space reads)"<<endl;

code/plugin_SequencesLoader/BzReader.cpp
@@ -19,7 +19,7 @@
 
 */
 
-#ifdef HAVE_LIBBZ2
+#ifdef CONFIG_HAVE_LIBBZ2
 
 #define __BzReader_MAXIMUM_LENGTH 2*4096
 

code/plugin_SequencesLoader/BzReader.h
@@ -22,7 +22,7 @@
 #ifndef _BzReader 
 #define _BzReader
 
-#ifdef HAVE_LIBBZ2
+#ifdef CONFIG_HAVE_LIBBZ2
 
 #include <bzlib.h>
 #include <stdint.h>
@@ -51,6 +51,6 @@ public:
 	void close();
 };
 
-#endif /* HAVE_LIBBZ2 */
+#endif
 
 #endif /* _BzReader */

code/plugin_SequencesLoader/FastqBz2Loader.cpp
@@ -19,7 +19,7 @@
 
 */
 
-#ifdef HAVE_LIBBZ2
+#ifdef CONFIG_HAVE_LIBBZ2
 
 #include<plugin_SequencesLoader/FastqBz2Loader.h>
 #include <stdlib.h>

code/plugin_SequencesLoader/FastqBz2Loader.h
@@ -21,7 +21,8 @@
 
 #ifndef _FastqBz2Loader
 #define _FastqBz2Loader
-#ifdef HAVE_LIBBZ2
+
+#ifdef CONFIG_HAVE_LIBBZ2
 
 #include<string>
 #include<vector>

code/plugin_SequencesLoader/Loader.cpp
@@ -108,7 +108,7 @@ int Loader::load(string file,bool isGenome){
 	}
 	#endif
 
-	#ifdef HAVE_LIBBZ2
+	#ifdef CONFIG_HAVE_LIBBZ2
 	if(file.substr(file.length()-10,10)==".fastq.bz2"){
 		m_type=FORMAT_FASTQ_BZ2;
 		int ret=m_fastqbz2.open(file,4);
@@ -174,7 +174,7 @@ void Loader::loadSequences(){
 	}else if(m_type==FORMAT_FASTQ){
 		m_fastq.load(m_maxToLoad,&m_reads,&m_allocator,4);
 	}else if(m_type==FORMAT_FASTQ_BZ2){
-		#ifdef HAVE_LIBBZ2
+		#ifdef CONFIG_HAVE_LIBBZ2
 		m_fastqbz2.load(m_maxToLoad,&m_reads,&m_allocator,4);
 		#endif
 	}else if(m_type==FORMAT_CSFASTA){
@@ -184,7 +184,7 @@ void Loader::loadSequences(){
 	}else if(m_type==FORMAT_FASTA){
 		m_fastq.load(m_maxToLoad,&m_reads,&m_allocator,2);
 	}else if(m_type==FORMAT_FASTA_BZ2){
-		#ifdef HAVE_LIBBZ2
+		#ifdef CONFIG_HAVE_LIBBZ2
 		m_fastqbz2.load(m_maxToLoad,&m_reads,&m_allocator,2);
 		#endif
 	}else if(m_type==FORMAT_FASTA_GZ){

code/plugin_SequencesLoader/Loader.h
@@ -34,7 +34,7 @@
 #include<plugin_SequencesLoader/FastqGzLoader.h>
 #endif
 
-#ifdef HAVE_LIBBZ2
+#ifdef CONFIG_HAVE_LIBBZ2
 #include<plugin_SequencesLoader/FastqBz2Loader.h>
 #endif
 
@@ -82,7 +82,7 @@ class Loader{
 	FastqGzLoader m_fastqgz;
 	#endif
 
-	#ifdef HAVE_LIBBZ2
+	#ifdef CONFIG_HAVE_LIBBZ2
 	FastqBz2Loader m_fastqbz2;
 	#endif
 