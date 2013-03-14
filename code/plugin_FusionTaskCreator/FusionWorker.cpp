acfec537c565e357a8c5227d61b4f25eaea1eb25
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-03-14 14:52:57 -0400

    add comments for old class
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- code/plugin_FusionTaskCreator/FusionWorker.cpp
+++ code/plugin_FusionTaskCreator/FusionWorker.cpp
@@ -1,6 +1,6 @@
 /*
- 	Ray
-    Copyright (C) 2010, 2011, 2012 Sébastien Boisvert
+    Ray -- Parallel genome assemblies for parallel DNA sequencing
+    Copyright (C) 2010, 2011, 2012, 2013 Sébastien Boisvert
 
 	http://DeNovoAssembler.SourceForge.Net/
 
@@ -270,6 +270,9 @@ WorkerHandle FusionWorker::getWorkerIdentifier(){
 	return m_workerIdentifier;
 }
 
+/**
+ * This class takes a path, and try to find another path that contains it.
+ */
 void FusionWorker::constructor(WorkerHandle number,GraphPath*path,PathHandle identifier,bool reverseStrand,
 	VirtualCommunicator*virtualCommunicator,Parameters*parameters,RingAllocator*outboxAllocator,
 
