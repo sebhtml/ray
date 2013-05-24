606be2a7a710a2261acc44ddd0ef4bc5008ae523
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-05-24 16:44:32 -0400

    fix the script to build Ray with LTO
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- scripts/Build-Link-Time-Optimization.sh
+++ scripts/Build-Link-Time-Optimization.sh
@@ -2,12 +2,12 @@
 
 
 mpicxx \
--Wall -std=c++98 -O3 -march=native -flto -fwhole-program \
+-Wall -std=c++98 -Os -march=native -flto -fwhole-program \
  -D CONFIG_HAVE_LIBZ -D CONFIG_HAVE_LIBBZ2 -lz -lbz2 \
  -flto-report \
- -D CONFIG_MAXKMERLENGTH=64  \
+ -D CONFIG_MAXKMERLENGTH=32  \
  -D CONFIG_RAY_VERSION=\"x.y.z\" -D CONFIG_RAYPLATFORM_VERSION=\"x.y.z\" \
- -I.  \
+ -I. -I RayPlatform  \
  $(find code/|grep .cpp$;find RayPlatform/|grep .cpp$) \
  -o Ray
 
