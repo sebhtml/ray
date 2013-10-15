cf670fcac67d3be693a85c5458e3d53b2249fb6a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-10-15 13:06:41 -0400

    add license to Ray Surveyor files
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- code/Surveyor/StoreKeeper.cpp
+++ code/Surveyor/StoreKeeper.cpp
@@ -1,3 +1,23 @@
+/*
+    Copyright 2013 Sébastien Boisvert
+    Copyright 2013 Université Laval
+    Copyright 2013 Centre Hospitalier Universitaire de Québec
+
+    This file is part of Ray Surveyor.
+
+    Ray Surveyor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    Ray Surveyor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ray Surveyor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "StoreKeeper.h"
 
